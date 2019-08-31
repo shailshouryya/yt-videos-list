9d6f78af17001c19b62b3000b0f081c03fda3f34
Shail Shouryya <shailshouryya@gmail.com>
2019-08-30 20:03:48 -0700

    created bs4 integration to scrape URLs and titles of videos on intial load page: this won't collect all videos yet, need selenium for that

---
+++ script.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+baseUrl = 'https://www.youtube.com'
+user = 'user/'
+userName = 'MattSDance/'
+videos = 'videos'
+
+channelVideosUrl = baseUrl + '/' + user + userName + videos
+soup = BeautifulSoup(requests.get(channelVideosUrl).content, 'lxml')
+
+with open ('{}VideosList.txt'.format(userName.strip('/')), 'w+') as f:
+	print('Opened       {}, writing to file...'.format(f.name))
+	for url in soup.find_all('a', attrs = {'class': 'yt-uix-sessionlink', 'dir':'ltr'}):
+# 		f.write('{}\n'.format(url))
+		f.write('title:\n{}\n'.format(url.string))
+		f.write('url.href:\n{}{}\n'.format(baseUrl, url.get('href')))
+		f.write('*'*50 + '\n')
+	print('Wrote to     {}, closing file...'.format(f.name))
+	
+	
+with open ('{}VideosList.csv'.format(userName.strip('/')), 'w+') as csvfile:
+	print('Opened       {}, writing to file...'.format(csvfile.name))
+	fieldnames = ['Video Title', 'URL', 'Watched?']
+	writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+# 	writer = csv.writer(f)
+	writer.writeheader()
+	
+	for url in soup.find_all('a', attrs = {'class': 'yt-uix-sessionlink', 'dir':'ltr'}):
+	    writer.writerow({'Video Title': f'{url.string}', 'URL': f'{baseUrl}{url.get("href")}', 'Watched?': ''})
+	
+	print('Wrote to     {}, closing file...'.format(csvfile.name))
