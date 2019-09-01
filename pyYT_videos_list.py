4f10f27bf35db7cba2106af6765edcc7799c08e3
Shail Shouryya <shailshouryya@gmail.com>
2019-09-01 15:40:20 -0700

    created format for simple, clean way to run code

---
+++ pyYT_videos_list.py
@@ -0,0 +1,14 @@
+import scrollAutomation
+
+class ListGenerator:
+    def __init__(self):
+        pass
+    
+    def generate_list(self):
+        pass
+
+def main():
+    scrollAutomation.main()
+        
+if __name__ == '__main__':
+    main()
