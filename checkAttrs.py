10e73e6327be92e5d64d69e52cf566ed64265cb3
Shail Shouryya <shailshouryya@gmail.com>
2019-08-31 20:22:23 -0700

    added comment indicating what line to change

--- checkAttrs.py
+++ checkAttrs.py
@@ -1,8 +1,11 @@
+# https://www.softwaretestingmaterial.com/how-to-locate-element-by-xpath-locator/
+# https://stackoverflow.com/questions/27307131/selenium-webdriver-how-do-i-find-all-of-an-elements-attributes
+
 from selenium import webdriver
 from pprint import pprint
 driver = webdriver.Firefox()
 driver.get('https://www.youtube.com/user/sentdex/videos')
 
-element = driver.find_element_by_xpath('//*[@id="video-title"]')
+element = driver.find_element_by_xpath('//*[@id="video-title"]') # CHANGE THIS LINE TO TEST ON DIFFERENT PAGE
 attrs = driver.execute_script('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', element)
 pprint(attrs)
