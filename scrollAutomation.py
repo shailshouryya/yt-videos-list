f31fe5c03a89233c597fe2ce764e3d533dd8b361
Shail Shouryya <shailshouryya@gmail.com>
2019-09-01 17:24:50 -0700

    added docstring indicating what arguments should be available to user from cli

--- scrollAutomation.py
+++ scrollAutomation.py
@@ -4,7 +4,26 @@ import time
 from pprint import pprint
 import csv
 
-
+def cli():
+    '''
+    Provides optional arguments for user increased visibility. Note that using some of the arguments may slow down run time and program may take slightly longer to execute.
+    '''
+    '''
+    
+    -c --channelName required, specify channel name
+    -o --overwrite overwrite file if a file of the same name already exists in the output directory
+    --version shows version number and exits
+    --csv writes to csv file
+    --txt writes to txt file
+    --docx writes to word dox (not yet available)
+    -v --verbose print every 10 videos written to file
+    -s --see open the selenium browser to see what's happening during program execution. Useful if not all videos are being found, allows you to see where the scrolling gets stuck
+    -q --quiet suppresses program updates, only prints to stdout when scrolling is complete files are opened and closed, and any errors that may occur
+    -h --help display information on usage and functionality
+    -p --pause change pause time between scrolls, set to 0.8s by default
+    -r --reverse reverse the indexing so oldest video starts at 1 and most recent video has highest index
+    '''
+    pass
 
 def scrollToBottom (user_name, seleniumInstance, scroll_pause_time=0.8):
     start = time.perf_counter()
