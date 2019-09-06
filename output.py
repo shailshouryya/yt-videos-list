2b26ac1cd86f5c353ced574ee60fe1c368786ad5
Shail Shouryya <shailshouryya@gmail.com>
2019-09-05 18:46:26 -0700

    modified class names to reflect changes in module run method

--- output.py
+++ output.py
@@ -4,7 +4,7 @@ class Common:
     '''
     noVideosFound = 'No videos were found for the name you provided. Are you sure you typed in the channel name correctly?\n'
 
-class pyYT_videos_listMessage(Common):
+class ModuleMessage(Common):
 
     runInHeadless = '\nAdvanced usage: you can run this program in headless mode with the optional "headless" parameter set to True to speed up execution slightly:'
     
@@ -16,7 +16,7 @@ class pyYT_videos_listMessage(Common):
     
     fileAlreadyExistsRerunUsage = 'Example usage:\n LG.generate_list(writeFormat="w")\n'
     
-class executeMessage(Common):
+class ScriptMessage(Common):
     inputMessage = "What is the name of the YouTube channel you want to generate the list for?" + "\n\n" + "If you're unsure, click on the channel and look at the URL." + "\n" + "It should be in the format:" + "\n" + "https://www.youtube.com/user/YourChannelName" + "\n" + "OR" + "\n" + "https://www.youtube.com/channel/YourChannelName" + "\n\n" + "Substitute what you see for YourChannelName and type it in below (NOTE: if your url looks like the second option, you need to run this script with the -c or --channel flag):\n"
     
     checkChannelType = 'If you did type the name in correctly, perhaps the channelType is set incorrectly. Try using the -c or --channelType flag for this script if you didn\'t do it when running this script, or try running the script without the -c or --channelType flag if you DID include that flag when running this script.'
