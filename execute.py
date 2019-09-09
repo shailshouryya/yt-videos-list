6655df19e03f8d6e5a58fe1274e6925e6dee4b5b
Shail Shouryya <shailshouryya@gmail.com>
2019-09-08 18:52:57 -0700

    moved execution logic to separate file

--- execute.py
+++ execute.py
@@ -1,179 +1,23 @@
+import program
 from output import Common, ModuleMessage, ScriptMessage
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 import time
-import csv
-import os
 
-cMessage = Common()
-sMessage = ScriptMessage()
-
-def checkFileExists(filename):
-    if os.path.isfile(f'./{filename}'):
-        return True
-    return False
-    
-def updateWriteFormat(channelName, fileType):
-    filename = f'{channelName}VideosList.{fileType}'
-    fileExists = checkFileExists(filename)
-    
-    def askUser(filename):
-        userResponse = input()
-        if 'proceed' in userResponse.strip().lower():
-            return 'w'
-        if 'skip' in userResponse.strip().lower():
-            return 0
-        else:
-            print ('\n' + cMessage.invalidResponse)
-            cMessage.fileAlreadyExistsPrompt(filename)
-            return askUser() 
-        
-    if fileExists is True:
-        cMessage.fileAlreadyExistsWarning(filename)
-        cMessage.fileAlreadyExistsPrompt(filename)
-        return askUser()
-    return 'x'
-        
-        
-        
-    
-            
-#             else:
-#                 print (f'You entered {userResponse}. You should have entered {userResponse.lower()}')
-#                 print ('\n' + cMessage.invalidResponse)
-#                 cMessage.fileAlreadyExistsPrompt(filename)
-#                 updatedUserResponse = input()
-#                 return determineWriteType(updatedUserResponse)
-        # if userResponse is None, the file does not exist so keep write format in default 'x' mode
-
-def scrollToBottom (channelName, channelType, seleniumInstance, scrollPauseTime):
-    start = time.perf_counter()
-    driver = seleniumInstance
-    
-    baseUrl = 'https://www.youtube.com'
-    videos = 'videos'
-    url = baseUrl + '/' + channelType + '/' + channelName + '/' + videos 
-    
-    driver.get(url)
-    elemsCount = driver.execute_script(
-    "return document.querySelectorAll('ytd-grid-video-renderer').length"
-    )
-    
-    while True:
-        driver.execute_script("window.scrollBy(0, 50000);")
-        time.sleep(scrollPauseTime)
-        new_elemsCount = driver.execute_script(
-        "return document.querySelectorAll('ytd-grid-video-renderer').length"
-        )
-        print (f'Found {new_elemsCount} videos...')
-    
-        if new_elemsCount == elemsCount:
-            # wait 0.6 seconds and check again to verify you really did reach the end of the page, and there wasn't a buffer loading period
-            print (cMessage.noNewVideosFound)
-            time.sleep(0.6)
-            new_elemsCount = driver.execute_script(
-            "return document.querySelectorAll('ytd-grid-video-renderer').length"
-            )
-            if new_elemsCount == elemsCount:
-                print('Reached end of page!')
-                break
-        elemsCount = new_elemsCount
-        
-    elements = driver.find_elements_by_xpath('//*[@id="video-title"]')
-    end = time.perf_counter()
-    functionTime = end - start - 2 # subtract 2 to account for the extra waiting time to verify end of page
-    print(f'It took {functionTime} to find all {len(elements)} videos from {url}\n')
-    return elements
-
-def writeToTxt (listOfVideos, channelName, writeFormat):
-    if writeFormat == 0:
-        return
-    start = time.perf_counter()
-    with open('{}VideosList.txt'.format(channelName.strip('/')), writeFormat) as f:
-        print (f'Opened {f.name}, writing video information to file....')
-        
-        spacing = '\n    ' # newline followed by 4 spaces
-        for index, element in enumerate(listOfVideos, 1):
-            f.write(f'Index:{spacing}{index}\n')
-            f.write(f'Watched?{spacing}\n')
-            f.write(f'Video Title:{spacing}{element.get_attribute("title")}\n')
-            f.write(f'Video URL:{spacing}{element.get_attribute("href")}\n')
-            f.write(f'Watch again later?{spacing}\n')
-            f.write(f'Notes:{spacing}\n')
-            
-            f.write('*'*75 + '\n')
-            if index % 250 == 0:
-                print (f'{index} videos written to {f.name}...')
-        print (f'Finished writing to {f.name}')
-        print (f'{index} videos written to {f.name}')
-        print (f'Closing {f.name}\n')
-        
-        end = time.perf_counter()
-        functionTime = end - start
-        print(f'It took {functionTime} to write all {index} videos to {f.name}\n')
-        
-def saveToMemWriteToTxt (listOfVideos, channelName, writeFormat):
-    if writeFormat == 0:
-        return
-    start = time.perf_counter()
-    with open('{}VideosList.txt'.format(channelName.strip('/')), writeFormat) as fm:
-        print (f'Opened {fm.name}, writing video information to file....')
-        
-        text = ''
-        spacing = '\n    ' # newline followed by 4 spaces
-        for index, element in enumerate(listOfVideos, 1):
-            text += f'Index:{spacing}{index}' + '\n'
-            text += f'Watched?{spacing}' + '\n'
-            text += f'Video Title:{spacing}{element.get_attribute("title")}' + '\n'
-            text += f'Video URL:{spacing}{element.get_attribute("href")}' + '\n'
-            text += f'Watch again later?{spacing}' + '\n'
-            text += f'Notes:{spacing}' + '\n'
-            
-            text += '*'*75 + '\n'
-            if index % 250 == 0:
-                print (f'{index} videos saved to memory...')
-        
-        print (f'Finished saving video information to memory')
-        fm.write(text)
-        print (f'{index} videos written to {fm.name}')
-        print (f'Closing {fm.name}\n')
-        end = time.perf_counter()
-        functionTime = end - start
-        print(f'It took {functionTime} to write all {index} videos to {fm.name}\n')
-
-def writeToCsv (listOfVideos, channelName, writeFormat):
-    if writeFormat == 0:
-        return
-    with open('{}VideosList.csv'.format(channelName.strip('/')), writeFormat) as csvfile:
-        print (f'Opened {csvfile.name}, writing video information to file....')
-        fieldnames = ['Index', 'Watched?', 'Video Title', 'Video URL', 'Watch again later?', 'Notes']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        
-        for index, element in enumerate(listOfVideos, 1):
-            writer.writerow(
-            {'Index': f'{index}', 'Watched?': '', 'Video Title': f'{element.get_attribute("title")}', 'Video URL': f'{element.get_attribute("href")}', 'Watch again later?': '', 'Notes': ''})
-            if index % 250 == 0:
-                print(f'{index} videos written to {csvfile.name}...')
-        print (f'Finished writing to {csvfile.name}')
-        print (f'{index} videos written to {csvfile.name}')
-        print (f'Closing {csvfile.name}\n')
-        
 def run(channelName, channelType, csv, csvWriteFormat, txt, txtWriteFormat, docx, docxWriteFormat, chronological, headless, scrollPauseTime, executionType):
     mMessage = ModuleMessage()
     channelName = channelName.strip().strip('/')
     channelType = channelType.strip().strip('/')
     
     if csv is True and csvWriteFormat == 'x':
-        csvWriteFormat = updateWriteFormat(channelName, 'csv')
+        csvWriteFormat = program.updateWriteFormat(channelName, 'csv')
 
     if txt is True and txtWriteFormat == 'x':
-        txtWriteFormat = updateWriteFormat(channelName, 'txt')
+        txtWriteFormat = program.updateWriteFormat(channelName, 'txt')
         
     if docx is True and docxWriteFormat == 'x' is True:
-        docxWriteFormat = updateWriteFormat(channelName, 'docx')
+        docxWriteFormat = program.updateWriteFormat(channelName, 'docx')
         
-    
     programStart = time.perf_counter()
     if headless is False: # opens Selenium browsing instance
         driver = webdriver.Firefox()
@@ -186,24 +30,17 @@ def run(channelName, channelType, csv, csvWriteFormat, txt, txtWriteFormat, docx
         driver = webdriver.Firefox(options=options)
         
     with driver:
-        videosList = scrollToBottom(channelName, channelType, driver, scrollPauseTime)
+        videosList = program.scrollToBottom(channelName, channelType, driver, scrollPauseTime)
         if len(videosList) == 0:
             print (cMessage.noVideosFound)
             print (mMessage.checkChannelType) if executionType == 'module' else print (sMessage.checkChannelType)
             return
         if csv is True:
-            writeToCsv(videosList, channelName, csvWriteFormat)
-#             except FileExistsError as e:
-#                 print (e)
-#                 print (mMessage.fileAlreadyExists)
-#                 print (mMessage.fileAlreadyExistsRerunUsage)
+            program.writeToCsv(videosList, channelName, csvWriteFormat)
         if txt is True:
-            writeToTxt(videosList, channelName, txtWriteFormat)
+            program.writeToTxt(videosList, channelName, txtWriteFormat)
                 # saveToMemWriteToTxt(videosList, channelName, writeFormat) # slightly slower than writing to disk directly
-#             except FileExistsError as e:
-#                 print (e)
-#                 print (mMessage.fileAlreadyExists)
-#                 print (mMessage.fileAlreadyExistsRerunUsage)
+                
     programEnd = time.perf_counter()
     totalTime = programEnd - programStart
     print(f'This program took {totalTime} seconds to complete.\n')
