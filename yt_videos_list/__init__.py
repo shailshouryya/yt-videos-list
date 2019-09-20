561caacd45ac27e7a04d942130b24a9b2dbbfc23
Shail Shouryya <shailshouryya@gmail.com>
2019-09-19 23:47:44 -0700

    added more info about package (version, author, development status, license, ideal python environment, and source url

--- yt_videos_list/__init__.py
+++ yt_videos_list/__init__.py
@@ -1,2 +1,21 @@
+'''
+Add program overview
+
+version: 0.1.0
+author: Shail-Shouryya
+development_status: 4 - Beta
+intended_audience: Developers, Hobbyists
+license:  OSI Approved :: Apache License 2.0
+ideal_python_version: Python 3.7
+source: https://github.com/Shail-Shouryya/yt_videos_list
+'''
+__version__ = "0.1.0"
+__author__ = 'Shail-Shouryya'
+__development_status__ = '4 - Beta'
+__intended_audience__ = 'Developers, Hobbyists'
+__license__ =  'OSI Approved :: Apache License 2.0'
+__ideal_python_version__ = 'Python 3.7'
+__source__ = 'https://github.com/Shail-Shouryya/yt_videos_list'
+
 import yt_videos_list
 from yt_videos_list.main import ListGenerator
