6cc3d3d0c92e8fec2ee85d167f15384b7bc7548d
Shail Shouryya <shailshouryya@gmail.com>
2019-10-05 23:23:50 -0700

    changed variable naming to CamelCasing, changed default scrollPauseTime to 0.8 seconds, and made readme more descriptive

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding='utf-8') as f:
 
 setup(
     name='yt_videos_list',
-    version='0.1.2',
+    version='0.1.3',
     description='Python package to extract YouTube video titles and URLs',
     long_description=long_description,
     long_description_content_type='text/markdown',
--- yt_videos_list/__init__.py
+++ yt_videos_list/__init__.py
@@ -1,7 +1,7 @@
 '''
 Add program overview
 
-version: 0.1.2
+version: 0.1.3
 author: Shail-Shouryya
 development_status: 4 - Beta
 intended_audience: Developers, Hobbyists
@@ -9,7 +9,7 @@ license:  OSI Approved :: Apache License 2.0
 ideal_python_version: Python 3.7
 source: https://github.com/Shail-Shouryya/yt_videos_list
 '''
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = 'Shail-Shouryya'
 __development_status__ = '4 - Beta'
 __intended_audience__ = 'Developers, Hobbyists'
