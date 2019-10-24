44b12545387c0e3c37cfd25fe30d83b4fb382ab6
Shail Shouryya <shailshouryya@gmail.com>
2019-10-23 23:57:46 -0700

    removed extra unnecessary comma at end of init parameters list - this should have never been there, not sure why it was here

--- yt_videos_list/__main__.py
+++ yt_videos_list/__main__.py
@@ -4,7 +4,7 @@ except:
     import script
 
 class ListGenerator:
-    def __init__(self, txt=True, txtWriteFormat='x', csv=True, csvWriteFormat='x', docx=False, docxWriteFormat='x', chronological=True, headless=False, scrollPauseTime=0.8,):
+    def __init__(self, txt=True, txtWriteFormat='x', csv=True, csvWriteFormat='x', docx=False, docxWriteFormat='x', chronological=True, headless=False, scrollPauseTime=0.8):
         '''
         Creates a list generator object with one required positional argument (channel) and one optional argument (channelType). The channelType is set to "user" by default, but if the YouTube channel you are looking at is a "channel" instead of "user" - you will need to change the default channelType parameter to "channel"
         Example usage:
