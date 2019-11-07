2bfc136c386abe7682964776c68435556a293f0b
Shail Shouryya <shailshouryya@gmail.com>
2019-11-06 21:48:42 -0800

    0.2.01: fix setup file for pip installation

--- yt_videos_list/__init__.py
+++ yt_videos_list/__init__.py
@@ -1,6 +1,6 @@
 from yt_videos_list.__main__ import ListGenerator
 '''
-version: 0.1.6
+version: 0.2.01
 author: Shail-Shouryya
 development_status: 4 - Beta
 intended_audience: Developers, Hobbyists
@@ -8,7 +8,7 @@ license:  OSI Approved :: Apache License 2.0
 ideal_python_version: Python 3.7
 source: https://github.com/Shail-Shouryya/yt_videos_list
 '''
-__version__ = "0.1.6"
+__version__ = "0.2.01"
 __author__ = 'Shail-Shouryya'
 __development_status__ = '4 - Beta'
 __intended_audience__ = 'Developers, Hobbyists'
