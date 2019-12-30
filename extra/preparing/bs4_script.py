ece96fc2fbb9d05c1818e26ba4af8455b67d5e88
Shail Shouryya <shailshouryya@gmail.com>
2019-12-29 20:32:16 -0800

    uses f-string formatting (and whitespace removal)

--- extra/preparing/bs4_script.py
+++ extra/preparing/bs4_script.py
@@ -3,11 +3,11 @@ import requests
 import csv
 
 baseUrl = 'https://www.youtube.com'
-user = 'user/'
-userName = 'MattSDance/'
+user = 'user'
+userName = 'MattSDance'
 videos = 'videos'
 
-channelVideosUrl = baseUrl + '/' + user + userName + videos
+channelVideosUrl = f'{baseUrl}/{user}/{userName}/{videos}'
 soup = BeautifulSoup(requests.get(channelVideosUrl).content, 'lxml')
 
 def writeToTxt():
@@ -19,7 +19,7 @@ def writeToTxt():
             f.write('url.href:\n{}{}\n'.format(baseUrl, url.get('href')))
             f.write('*'*50 + '\n')
         print('Wrote to     {}, closing file...'.format(f.name))
-    
+
 def writeToCsv():
     with open ('{}VideosList.csv'.format(userName.strip('/')), 'w+') as csvfile:
         print('Opened       {}, writing to file...'.format(csvfile.name))
@@ -27,12 +27,12 @@ def writeToCsv():
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         # writer = csv.writer(f)
         writer.writeheader()
-    
+
         for url in soup.find_all('a', attrs = {'class': 'yt-uix-sessionlink', 'dir':'ltr'}):
             writer.writerow({'Video Title': f'{url.string}', 'URL': f'{baseUrl}{url.get("href")}', 'Watched?': ''})
-    
+
         print('Wrote to     {}, closing file...'.format(csvfile.name))
-    
+
 def main():
     writeToTxt()
     writeToCsv()
