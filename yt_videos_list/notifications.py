8a97b0752191e5fb76d931ddc2c6a6b864684000
Shail Shouryya <shailshouryya@gmail.com>
2020-01-05 20:09:20 -0800

    renames driverDownloadsListForOS->driverDownloadsForOS

--- yt_videos_list/notifications.py
+++ yt_videos_list/notifications.py
@@ -27,7 +27,7 @@ class Common:
 
         print (geckodriverDownloadInstructions) if userBrowser == 'firefox' else print (operadriverDownloadInstructions) if userBrowser == 'opera' else print (chromedriverDownloadInstructions) if userBrowser == 'chrome' else print ('This is an OS specific browser.')
 
-        driverDownloadsListForOS = {
+        driverDownloadsForOS = {
             'firefox': {
                 'macos': [
                     '# macos geckodriver (Firefoxdriver) v0.26.0',
@@ -206,7 +206,7 @@ class Common:
             }
         }
 
-        for driverVersionDownload in driverDownloadsListForOS[userBrowser][userOS]:
+        for driverVersionDownload in driverDownloadsForOS[userBrowser][userOS]:
             print (driverVersionDownload)
 
         def displayMoreDependencyInformation(userBrowser):
