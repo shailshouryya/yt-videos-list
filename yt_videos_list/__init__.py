3bc59c986fc2947aad5e2dc27bf77e6c86320aa9
Shail Shouryya <shailshouryya@gmail.com>
2020-03-03 23:12:56 -0800

    clarifies scrollPauseTime is modified in units of seconds

--- yt_videos_list/__init__.py
+++ yt_videos_list/__init__.py
@@ -58,7 +58,7 @@ class ListGenerator:
              -> headless=False (default) OR headless=True
 
         Options for the `scrollPauseTime argument` are any float values greater than 0 (default 0.8).
-          * The value you provide will be how long the program waits before trying to scroll the videos list page down for the channel you want to scrape.
+          * The value you provide will be how long (in seconds) the program waits before trying to scroll the videos list page down for the channel you want to scrape.
           * For fast internet connections, you may want to reduce the value, and for slow connections you may want to increase the value.
              -> scrollPauseTime=0.8 (default)
           * CAUTION: reducing this value too much will result in the programming not capturing all the videos, so be careful! Experiment :)
