95fc2fdcd9c06bf2db96ab65b91df4768824a5e5
Shail Shouryya <shailshouryya@gmail.com>
2020-03-15 18:03:07 -0700

    changes ideal python version from 3.7 -> 3.6+

--- yt_videos_list/__init__.py
+++ yt_videos_list/__init__.py
@@ -6,7 +6,7 @@ author: Shail-Shouryya
 development_status: 4 - Beta
 intended_audience: Developers, Hobbyists
 license:  OSI Approved :: Apache License 2.0
-ideal_python_version: Python 3.7
+ideal_python_version: Python 3.6+
 source: https://github.com/Shail-Shouryya/yt_videos_list
 '''
 __version__ = '0.2.16'
@@ -14,7 +14,7 @@ __author__ = 'Shail-Shouryya'
 __development_status__ = '4 - Beta'
 __intended_audience__ = 'Developers, Hobbyists'
 __license__ =  'OSI Approved :: Apache License 2.0'
-__ideal_python_version__ = 'Python 3.7'
+__ideal_python_version__ = 'Python 3.6+'
 __source__ = 'https://github.com/Shail-Shouryya/yt_videos_list'
 
 class ListGenerator:
