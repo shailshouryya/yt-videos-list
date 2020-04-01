4dd0fbafaa1d381a4227e0cb0c2f82b320a9cee0
Shail Shouryya <shailshouryya@gmail.com>
2020-04-01 03:36:40 -0700

    reformats whitespace in checkAttrs.py for increased readability

--- extra/preparing/checkAttrs.py
+++ extra/preparing/checkAttrs.py
@@ -9,5 +9,11 @@ driver = webdriver.Firefox()
 driver.get('https://www.youtube.com/user/sentdex/videos')
 
 element = driver.find_element_by_xpath('//*[@id="video-title"]') # CHANGE THIS LINE TO TEST ON DIFFERENT PAGE
-attrs = driver.execute_script('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', element)
+attrs = driver.execute_script(
+    'var items = {};                                                                          \
+    for (index = 0; index < arguments[0].attributes.length; ++index) {                        \
+            items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value \
+        };                                                                                    \
+        return items;', element
+    )
 pprint(attrs)
