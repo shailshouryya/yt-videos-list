3c184443ab38ac65f00376d0228ea52e080cdf66
Shail Shouryya <shailshouryya@gmail.com>
2020-04-01 03:22:45 -0700

    removes newline between comment and the line the comment refers to

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
 
     # If there are data files included in your packages that need to be installed, specify them here.
     # If using Python 2.6 or earlier, then these have to be included in MANIFEST.in as well.
-
     package_data={  # Optional
         # 'sample': ['package_data.dat'],
     },
