857c3754eed6f7162f28e9a54eeba6c643e05233
Shail Shouryya <shailshouryya@gmail.com>
2020-04-25 15:40:45 -0700

    0.2.17: refactoring + API change
    - no change in functionality, just changes in how things work under the hood and how to instantiate instances and call methods
      - updated ListGenerator() -> ListCreator()
        - no python generators are used in this program, so renamed to avoid being misleading and avoid potential confusion
      - updated create_list() -> create_list_for()
        - this is a more clear and obvious name that clearly describes what the method does
      - everything else is under the hood changes to how functions are invoked, user input is checked, functions/variables are named, code is organized (whitespace), import statements are organized, and user input is loaded (via tuple packing into instance_attributes in __init__.py and unpacking when loaded into execute.logic())

--- yt_videos_list/__init__.py
+++ yt_videos_list/__init__.py
@@ -2,7 +2,7 @@ from . import execute
 
 
 '''
-version:              0.2.15
+version:              0.2.17
 author:               Shail-Shouryya
 development_status:   4 - Beta
 intended_audience:    Developers, Hobbyists
@@ -12,7 +12,7 @@ source:               https://github.com/Shail-Shouryya/yt_videos_list
 '''
 
 
-__version__              = '0.2.16'
+__version__              = '0.2.17'
 __author__               = 'Shail-Shouryya'
 __development_status__   = '4 - Beta'
 __intended_audience__    = 'Developers, Hobbyists'
