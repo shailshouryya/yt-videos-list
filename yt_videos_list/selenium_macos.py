cab13283c2644c90780fcb5220071b03d3754665
Shail Shouryya <shailshouryya@gmail.com>
2020-04-29 03:14:21 -0700

    adds core download functions

--- yt_videos_list/selenium_macos.py
+++ yt_videos_list/selenium_macos.py
@@ -1,6 +1,15 @@
 import sys
 
 
+def download_geckodriver(version):
+    pass
+
+def download_operadriver(version):
+    pass
+
+def download_chromedriver(version):
+    pass
+
 def download(user_driver):
     print(f'Automatic Selenium dependency download for MacOS is not yet supported. Please follow the instructions below to set up the correct selenium dependecy for the {user_driver} driver.')
     sys.exit()
