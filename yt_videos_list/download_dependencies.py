d21948f20eda8adfec8796a4ca2ef901c3ee5971
Shail Shouryya <shailshouryya@gmail.com>
2020-05-02 02:10:26 -0700

    moves common logic for OSs from selenium_macos.py -> download_dependencies.py

--- yt_videos_list/download_dependencies.py
+++ yt_videos_list/download_dependencies.py
@@ -74,6 +74,29 @@ def execute_download_command(driver, user_os, version):
     print(f'{common_message.driver_downloads_for_os[driver][user_os][row]}')
     os.system(common_message.driver_downloads_for_os[driver][user_os][row])
 
+def download_all_dependencies(user_os):
+    common_message = Common()
+    selenium_user_os = globals()[f'selenium_{user_os}']
+    application_name = {
+        'macos': {
+            # 'driver': 'browser_name'
+            'firefox':  'Firefox',
+            'opera':    'Opera',
+            'chrome':   'Google Chrome'
+        }
+    }
+
+    for driver in application_name[user_os]:
+        browser = application_name[user_os][driver]
+        if selenium_user_os.browser_exists(browser):
+            full_version_number = selenium_user_os.get_browser_version(browser)
+            common_message.display_browser_found_information(browser, full_version_number)
+            major_version = full_version_number.split('.')[0]
+            selenium_user_os.download_driver(driver, major_version)
+        else:
+            common_message.display_browser_not_found_information(browser, full_version_number)
+
+
 def run():
     user_os = determine_user_os()
-    globals()[f'selenium_{user_os}'].download_all_dependencies()
+    download_all_dependencies(user_os)
--- yt_videos_list/selenium_macos.py
+++ yt_videos_list/selenium_macos.py
@@ -19,22 +19,3 @@ def download_driver(driver, version):
 def download(user_driver):
     print(f'Automatic Selenium dependency download for MacOS is not yet supported. Please follow the instructions below to set up the correct selenium dependecy for the {user_driver} driver.')
     sys.exit()
-
-def download_all_dependencies():
-    common_message = Common()
-    application_name = {
-        # 'driver': 'browser_name'
-        'firefox':  'Firefox',
-        'opera':    'Opera',
-        'chrome':   'Google Chrome'
-    }
-
-    for driver in application_name:
-        browser = application_name[driver]
-        if browser_exists(browser):
-            full_version_number = get_browser_version(browser)
-            common_message.display_browser_found_information(browser, full_version_number)
-            major_version = full_version_number.split('.')[0]
-            download_driver(driver, major_version)
-        else:
-            common_message.display_browser_not_found_information(browser, full_version_number)
