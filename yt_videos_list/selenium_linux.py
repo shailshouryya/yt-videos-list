f9ee4386be847fd98d2d2b793ffdf8028d58e3d5
Shail Shouryya <shailshouryya@gmail.com>
2020-05-02 16:51:34 -0700

    adds functions that need to be used in selenium_[windows|linux].py

---
+++ yt_videos_list/selenium_linux.py
@@ -0,0 +1,13 @@
+import re
+import sys
+import subprocess
+
+def browser_exists(browser):
+    # return browser in subprocess.getoutput('ls /Applications')
+    sys.exit()
+
+def get_browser_version(browser):
+    # with open (f'/Applications/{browser}.app/Contents/Info.plist') as f:
+    #     info_plist = f.read()
+    # return re.search('<key>CFBundleShortVersionString</key>\s*<string>([0-9\.]+)', info_plist)[1]
+    sys.exit()
