6f14c34dc5b4fe5838b74b2281ee5a37c7b25a41
Shail Shouryya <shailshouryya@gmail.com>
2020-05-03 16:51:42 -0700

    structures helper functions to find browsers in windows

--- yt_videos_list/selenium_windows.py
+++ yt_videos_list/selenium_windows.py
@@ -2,25 +2,21 @@ import re
 import sys
 import subprocess
 
-<<<<<<< Updated upstream
-=======
 
-USER = subprocess.getoutput("whoami").split('\\')[1]
+def firefox_exists():
+    pass
 
-def firefox_exists(browser):
-    return browser in r'more "C:\Program Files"'
+def opera_exists():
+    pass
 
-def opera_exists(browser):
-    return browser in rf'C:\Users\{USER}\AppData\Local\Programs'
+def chrome_exists():
+    pass
 
-def chrome_exists(browser):
-    return browser in rf'dir "C:\Program Files (x86)\Google"'
-
-
->>>>>>> Stashed changes
 def browser_exists(browser):
     # return browser in subprocess.getoutput('ls /Applications')
-    sys.exit()
+    if   browser == 'Mozilla Firefox': return firefox_exists()
+    elif browser == 'Opera':           return opera_exists()
+    elif browser == 'Google':          return chrome_exists()
 
 def get_browser_version(browser):
     # with open (f'/Applications/{browser}.app/Contents/Info.plist') as f:
