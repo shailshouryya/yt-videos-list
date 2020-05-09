8973053876dc3c3cdbffd4a3e12e8fef32bec3c7
Shail Shouryya <shailshouryya@gmail.com>
2020-05-09 16:03:06 -0700

    adds package initialization to test

--- tests/general.py
+++ tests/general.py
@@ -1,6 +1,31 @@
 import os
+import platform
 
+from yt_videos_list.windows import get_drive_letter
 from yt_videos_list import ListCreator
+
+os.system('pip install .')
+
+def remove_dependencies():
+    if platform.system().lower().startswith('windows'):
+        drive = get_drive_letter()
+        if os.path.exists(rf'{drive}:\Windows\geckodriver'):
+            os.remove(rf'{drive}:\Windows\geckodriver')
+        if os.path.exists(rf'{drive}:\Windows\operadriver'):
+            os.remove(rf'{drive}:\Windows\operadriver')
+        if os.path.exists(rf'{drive}:\Windows\chromedriver'):
+            os.remove(rf'{drive}:\Windows\chromedriver')
+        if os.path.exists(rf'{drive}:\Windows\bravedriver'):
+            os.remove(rf'{drive}:\Windows\bravedriver')
+    else:
+        if os.path.exists(r'/usr/local/bin/geckodriver'):
+            os.remove(r'/usr/local/bin/geckodriver')
+        if os.path.exists(r'/usr/local/bin/operadriver'):
+            os.remove(r'/usr/local/bin/operadriver')
+        if os.path.exists(r'/usr/local/bin/chromedriver'):
+            os.remove(r'/usr/local/bin/chromedriver')
+        if os.path.exists(r'/usr/local/bin/bravedriver'):
+            os.remove(r'/usr/local/bin/bravedriver')
 
 
 def delete_schafer5_file_if_exists():
@@ -16,7 +41,7 @@ lc_safari  = ListCreator(driver='safari')
 lc_chrome  = ListCreator(driver='chrome')
 lc_brave   = ListCreator(driver='brave')
 
-
+remove_dependencies()
 delete_schafer5_file_if_exists()
 lc_firefox.create_list_for('youtube.com/user/schafer5')
 delete_schafer5_file_if_exists()
