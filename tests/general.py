f7c909c6ae281b80924e33b07b0fce110a4c978b
Shail Shouryya <shailshouryya@gmail.com>
2020-05-09 20:25:03 -0700

    moves logic into main() function (tests/general.py)

--- tests/general.py
+++ tests/general.py
@@ -35,28 +35,32 @@ def delete_schafer5_file_if_exists():
     if os.path.exists('schafer5VideosList.csv'):
         os.remove('schafer5VideosList.csv')
 
+def main():
+    lc_firefox = ListCreator(driver='firefox')
+    lc_opera   = ListCreator(driver='opera')
+    lc_safari  = ListCreator(driver='safari')
+    lc_chrome  = ListCreator(driver='chrome')
+    lc_brave   = ListCreator(driver='brave')
 
-lc_firefox = ListCreator(driver='firefox')
-lc_opera   = ListCreator(driver='opera')
-lc_safari  = ListCreator(driver='safari')
-lc_chrome  = ListCreator(driver='chrome')
-lc_brave   = ListCreator(driver='brave')
-
-remove_dependencies()
-delete_schafer5_file_if_exists()
-lc_firefox.create_list_for('youtube.com/user/schafer5')
-delete_schafer5_file_if_exists()
-lc_opera.create_list_for  ('youtube.com/user/schafer5')
-if PLATFORM == 'darwin':
+    remove_dependencies()
     delete_schafer5_file_if_exists()
-    lc_safari.create_list_for ('youtube.com/user/schafer5')
-delete_schafer5_file_if_exists()
-lc_chrome.create_list_for ('youtube.com/user/schafer5')
-delete_schafer5_file_if_exists()
-lc_brave.create_list_for ('youtube.com/user/schafer5')
+    lc_firefox.create_list_for('youtube.com/user/schafer5')
+    delete_schafer5_file_if_exists()
+    lc_opera.create_list_for  ('youtube.com/user/schafer5')
+    if PLATFORM == 'darwin':
+        delete_schafer5_file_if_exists()
+        lc_safari.create_list_for ('youtube.com/user/schafer5')
+    delete_schafer5_file_if_exists()
+    lc_chrome.create_list_for ('youtube.com/user/schafer5')
+    delete_schafer5_file_if_exists()
+    lc_brave.create_list_for ('youtube.com/user/schafer5')
+
+# add these later
+# lc_firefox.headless = True
+# lc_opera.headless = True
+# lc_safari.headless = True
+# lc_chrome.headless = True
 
 
-lc_firefox.headless = True
-lc_opera.headless = True
-lc_safari.headless = True
-lc_chrome.headless = True
+if __name__ == '__main__':
+    main()
