16eed357c222ee0a843acf4fb3660bc745193e3a
Shail Shouryya <shailshouryya@gmail.com>
2020-05-10 14:11:51 -0700

    adds test to verify driver_downloads_for_os output

---
+++ tests/test_notifications.py
@@ -0,0 +1,12 @@
+from yt_videos_list.notifications import Common
+
+def main():
+    common_messages = Common()
+    drivers_dictionary = common_messages.driver_downloads_for_os
+    for driver in drivers_dictionary:
+        for os_commands in drivers_dictionary[driver]:
+            for line in drivers_dictionary[driver][os_commands]:
+                print(line)
+
+if __name__ == '__main__':
+    main()
