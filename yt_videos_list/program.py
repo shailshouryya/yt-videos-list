bd23f252c35ccbcce93c219d63500eab47297a4a
Shail Shouryya <shailshouryya@gmail.com>
2020-05-17 20:27:16 -0700

    adds high level overview of control flow for updating/creating

--- yt_videos_list/program.py
+++ yt_videos_list/program.py
@@ -1,13 +1,23 @@
-from . import create_file
+import os
+
+from . import create_file, update_file
 from .notifications import Common
 
 
 COMMON_MESSAGE = Common()
 
 def determine_action(url, driver, scroll_pause_time, chronological, file_name, txt, txt_write_format, csv, csv_write_format):
-    videos_list = create_file.scroll_to_bottom(url, driver, scroll_pause_time)
+    txt_exists = os.path.isfile(f'{file_name}.txt')
+    csv_exists = os.path.isfile(f'{file_name}.csv')
+    if txt_exists and csv_exists: videos_list = update_file.scroll_to_old_videos(url, driver, scroll_pause_time, txt_exists, csv_exists, file_name)
+    else:                         videos_list = create_file.scroll_to_bottom    (url, driver, scroll_pause_time) # run when either the txt or csv file needs to be created from scratch
     if len(videos_list) == 0:
         print(COMMON_MESSAGE.no_videos_found)
         return
-    if txt is True: create_file.write_to_txt(videos_list, file_name, txt_write_format, chronological)
-    if csv is True: create_file.write_to_csv(videos_list, file_name, csv_write_format, chronological)
+
+    if txt is True:
+        if txt_exists: update_file.write_to_txt(videos_list, file_name, txt_write_format, chronological)
+        else:          create_file.write_to_txt(videos_list, file_name, txt_write_format, chronological)
+    if csv is True:
+        if csv_exists: update_file.write_to_csv(videos_list, file_name, csv_write_format, chronological)
+        else:          create_file.write_to_csv(videos_list, file_name, csv_write_format, chronological)
