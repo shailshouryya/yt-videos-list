08244f2884a1949f9f07292b57388e15d79c1f03
Shail Shouryya <shailshouryya@gmail.com>
2020-05-26 01:32:38 -0700

    uses test_shared.py functions in test_msedgedriver.py
    - ↑ DRY and code-sharing

--- tests/test_msedgedriver.py
+++ tests/test_msedgedriver.py
@@ -1,12 +1,12 @@
-from yt_videos_list import ListCreator
-
-from test_shared import delete_schafer5_file_if_exists
+from test_shared import create_test_cases, run_test_case
 
 
 def main():
-    schafer5_url = 'youtube.com/user/schafer5'
-    delete_schafer5_file_if_exists()
-    ListCreator(driver='edge').create_list_for(schafer5_url)
+    browsers   = ['edge']
+    test_cases = create_test_cases(browsers)
+
+    for test_case in test_cases:
+        run_test_case(test_case)
 
 
 if __name__ == '__main__':
