07c975dde7a040a2d71e87226062b5b70957db7c
Shail Shouryya <shailshouryya@gmail.com>
2020-05-26 01:34:56 -0700

    logs message after tests finish in run_tests.[sh|bat] scripts

--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -2,3 +2,6 @@ python tests\test_cross_platform.py
 python tests\test_safaridriver.py
 python tests\test_msedgedriver.py
 python tests\test_notifications.py > tests\drivers_list_output.log
+
+echo "Finished running unit tests!"
+echo "Now exiting run_tests.bat ...."
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,3 +2,6 @@ python3 tests/test_cross_platform.py
 python3 tests/test_safaridriver.py
 python3 tests/test_msedgedriver.py
 python3 tests/test_notifications.py > tests/drivers_list_output.log
+
+echo "Finished running unit tests!"
+echo "Now exiting run_tests.sh ...."
