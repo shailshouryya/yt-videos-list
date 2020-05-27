d90cb640ea98f0ac86aff38153edbe981a41a8f5
Shail Shouryya <shailshouryya@gmail.com>
2020-05-27 01:48:06 -0700

    renames f -> file to conform to snake_case naming style

--- tests/test_dependencies_json.py
+++ tests/test_dependencies_json.py
@@ -8,18 +8,18 @@ from yt_videos_list.notifications import Common
 def main():
     common_messages = Common()
     drivers_dictionary = common_messages.driver_downloads_for_os
-    with open('temp.json', 'w') as f:
-        f.write('{\n')
+    with open('temp.json', 'w') as file:
+        file.write('{\n')
         for driver in drivers_dictionary:
-            f.write(f'  "{driver}": ' + '{\n')
+            file.write(f'  "{driver}": ' + '{\n')
             for supproted_os in drivers_dictionary[driver]:
-                f.write(f'    "{supproted_os}": [\n')
+                file.write(f'    "{supproted_os}": [\n')
                 for command in drivers_dictionary[driver][supproted_os]:
                     command = command.replace('\n', '').replace('"', "'")
-                    f.write(f'      "{command}",\n')
-                f.write('    ],\n')
-            f.write('  },\n')
-        f.write('}')
+                    file.write(f'      "{command}",\n')
+                file.write('    ],\n')
+            file.write('  },\n')
+        file.write('}')
     with open('temp.json', 'r') as ftemp, open('dependencies.json', 'w') as ffinal:
         formatted = re.sub(',\n    ]', '\n    ]', ftemp.read())
         formatted = re.sub('],\n  },', ']\n  },', formatted)
