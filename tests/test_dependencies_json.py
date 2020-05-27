5b74da3f2c2496785c9a145a81c8e016611482f3
Shail Shouryya <shailshouryya@gmail.com>
2020-05-27 01:28:05 -0700

    replaces drivers_list_output.log with dependencies.json

--- tests/test_dependencies_json.py
+++ tests/test_dependencies_json.py
@@ -7,11 +7,18 @@ from yt_videos_list.notifications import Common
 def main():
     common_messages = Common()
     drivers_dictionary = common_messages.driver_downloads_for_os
-    for driver in drivers_dictionary:
-        print (driver)
-        for os_commands in drivers_dictionary[driver]:
-            for line in drivers_dictionary[driver][os_commands]:
-                print(line)
+    with open('dependencies.json', 'w') as f:
+        f.write('{\n')
+        for driver in drivers_dictionary:
+            f.write(f'  "{driver}": ' + '{\n')
+            for os in drivers_dictionary[driver]:
+                f.write(f'    "{os}": [\n')
+                for command in drivers_dictionary[driver][os]:
+                    command = command.replace('\n', '').replace('"', "'")
+                    f.write(f'      "{command}",\n')
+                f.write('    ],\n')
+            f.write('  },\n')
+        f.write('}')
 
 if __name__ == '__main__':
     main()
