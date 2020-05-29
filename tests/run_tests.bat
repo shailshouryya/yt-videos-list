12fdd9a98ac9a74e078c658e084705ce7d18de95
Shail Shouryya <shailshouryya@gmail.com>
2020-05-29 01:17:08 -0700

    updates test script paths
    - missed from commit 1545b59653a0c156d7a46ab4ffca44fb98d26652

--- tests/run_tests.bat
+++ tests/run_tests.bat
@@ -1,7 +1,7 @@
 python tests\test_cross_platform.py
 python tests\test_safaridriver.py
 python tests\test_msedgedriver.py
-python tests\test_notifications.py > tests\drivers_list_output.log
+python tests\test_dependencies_json.py
 
 echo "Finished running unit tests!"
 echo "Now exiting run_tests.bat ...."
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,7 @@
 python3 tests/test_cross_platform.py
 python3 tests/test_safaridriver.py
 python3 tests/test_msedgedriver.py
-python3 tests/test_notifications.py > tests/drivers_list_output.log
+python3 tests/test_dependencies_json.py
 
 echo "Finished running unit tests!"
 echo "Now exiting run_tests.sh ...."
