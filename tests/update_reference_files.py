7b7c18ea17d1ebb367b67ff0ade24b452431887f
Shail Shouryya <shailshouryya@gmail.com>
2020-05-29 22:05:30 -0700

    updates reference files when run
    - without this the hash check in the verify_update() function in test_shared.py module will compare the output of the unit test to an outdated refernce file and result in a false mismatch

---
+++ tests/update_reference_files.py
@@ -0,0 +1,31 @@
+import os
+
+from yt_videos_list import ListCreator
+from test_shared import determine_user_os
+
+
+def update_schafer5_chronological():
+    ListCreator(reverse_chronological=False).create_list_for('youtube.com/user/schafer5', file_name='full_schafer5_chronological')
+
+
+def update_schafer5_non_chronological():
+    ListCreator(reverse_chronological=True).create_list_for('youtube.com/user/schafer5', file_name='full_schafer5_non_chronological') # reverse_chronological is already True by default, just included here to be explicit
+
+
+def move_reference_files_to_tests():
+    if determine_user_os() == 'windows': path_slash = '\\'
+    else:                                path_slash = '/'
+    os.replace('full_schafer5_chronological.txt', f'tests{path_slash}full_schafer5_chronological.txt')
+    os.replace('full_schafer5_chronological.csv', f'tests{path_slash}full_schafer5_chronological.csv')
+    os.replace('full_schafer5_non_chronological.txt', f'tests{path_slash}full_schafer5_non_chronological.txt')
+    os.replace('full_schafer5_non_chronological.csv', f'tests{path_slash}full_schafer5_non_chronological.csv')
+
+
+def main():
+    update_schafer5_chronological()
+    update_schafer5_non_chronological()
+    move_reference_files_to_tests()
+
+
+if __name__ == '__main__':
+    main()
