f11abede5cbccf5e6452ddfd1a3c09808905366b
Shail Shouryya <shailshouryya@gmail.com>
2020-06-15 00:46:35 -0700

    reorders functions to be readable top-to-bottom (test_shared.py)

--- python3.6+/tests/test_shared.py
+++ python3.6+/tests/test_shared.py
@@ -19,9 +19,14 @@ def create_test_cases(browsers):
     ]
 
 
-def delete_file(filepath, extension):
-    if os.path.exists(f'{filepath}.{extension}'):
-        os.remove(f'{filepath}.{extension}')
+def run_test_case(list_creator):
+    path_slash = determine_path_slash()
+    schafer5_url = 'youtube.com/user/schafer5'
+    delete_all_schafer5_files()
+    list_creator.create_list_for(schafer5_url)
+    if getattr(list_creator, 'reverse_chronological'): verify_update(list_creator, schafer5_url, f'tests{path_slash}partial_schafer5_reverse_chronological', f'tests{path_slash}full_schafer5_reverse_chronological')
+    else:                                              verify_update(list_creator, schafer5_url, f'tests{path_slash}partial_schafer5_chronological',         f'tests{path_slash}full_schafer5_chronological')
+
 
 def delete_all_schafer5_files():
     schafer5 = 'CoreySchafer_videos_list'
@@ -30,14 +35,9 @@ def delete_all_schafer5_files():
     delete_file(schafer5, 'md' )
 
 
-def run_test_case(list_creator):
-    path_slash = determine_path_slash()
-    schafer5_url = 'youtube.com/user/schafer5'
-    delete_all_schafer5_files()
-    list_creator.create_list_for(schafer5_url)
-    if getattr(list_creator, 'reverse_chronological'): verify_update(list_creator, schafer5_url, f'tests{path_slash}partial_schafer5_reverse_chronological', f'tests{path_slash}full_schafer5_reverse_chronological')
-    else:                                              verify_update(list_creator, schafer5_url, f'tests{path_slash}partial_schafer5_chronological',     f'tests{path_slash}full_schafer5_chronological')
-
+def delete_file(filepath, extension):
+    if os.path.exists(f'{filepath}.{extension}'):
+        os.remove(f'{filepath}.{extension}')
 
 def verify_update(driver, schafer5_url, test_file, full_file):
     variations = [use_partial_csv_only, use_partial_txt_only, use_partial_md_only, use_partial_csv_txt_and_md]
