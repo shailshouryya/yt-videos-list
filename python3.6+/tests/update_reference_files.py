e02137f827d022c80fef00d8678ca795d869e5fe
Shail Shouryya <shailshouryya@gmail.com>
2020-06-14 20:29:19 -0700

    renames non_chronological → reverse_chronological
    - missed updating this part of update_reference_files.py in both commits 4a72ec265b65555c5360cd97a4cc085985ea97c9 and 46fecb0bfba69a642496dea35c9931b7af10eac5

--- python3.6+/tests/update_reference_files.py
+++ python3.6+/tests/update_reference_files.py
@@ -9,15 +9,15 @@ def update_schafer5_chronological():
 
 
 def update_schafer5_non_chronological():
-    ListCreator(reverse_chronological=True).create_list_for('youtube.com/user/schafer5', file_name='full_schafer5_non_chronological') # reverse_chronological is already True by default, just included here to be explicit
+    ListCreator(reverse_chronological=True).create_list_for('youtube.com/user/schafer5', file_name='full_schafer5_reverse_chronological') # reverse_chronological is already True by default, just included here to be explicit
 
 
 def move_reference_files_to_tests():
     path_slash = determine_path_slash()
-    os.replace('full_schafer5_chronological.txt',     f'tests{path_slash}full_schafer5_chronological.txt')
-    os.replace('full_schafer5_chronological.csv',     f'tests{path_slash}full_schafer5_chronological.csv')
-    os.replace('full_schafer5_non_chronological.txt', f'tests{path_slash}full_schafer5_reverse_chronological.txt')
-    os.replace('full_schafer5_non_chronological.csv', f'tests{path_slash}full_schafer5_reverse_chronological.csv')
+    os.replace('full_schafer5_chronological.txt',         f'tests{path_slash}full_schafer5_chronological.txt')
+    os.replace('full_schafer5_chronological.csv',         f'tests{path_slash}full_schafer5_chronological.csv')
+    os.replace('full_schafer5_reverse_chronological.txt', f'tests{path_slash}full_schafer5_reverse_chronological.txt')
+    os.replace('full_schafer5_reverse_chronological.csv', f'tests{path_slash}full_schafer5_reverse_chronological.csv')
 
 
 def main():
