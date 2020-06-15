a14bfdf87cf288038dfdc3cd5ac4be4fd6be4cd2
Shail Shouryya <shailshouryya@gmail.com>
2020-06-14 21:27:50 -0700

    adds markdown reference file in tests

--- python3.6+/tests/update_reference_files.py
+++ python3.6+/tests/update_reference_files.py
@@ -16,8 +16,10 @@ def move_reference_files_to_tests():
     path_slash = determine_path_slash()
     os.replace('full_schafer5_chronological.txt',         f'tests{path_slash}full_schafer5_chronological.txt')
     os.replace('full_schafer5_chronological.csv',         f'tests{path_slash}full_schafer5_chronological.csv')
+    os.replace('full_schafer5_chronological.md',          f'tests{path_slash}full_schafer5_chronological.md')
     os.replace('full_schafer5_reverse_chronological.txt', f'tests{path_slash}full_schafer5_reverse_chronological.txt')
     os.replace('full_schafer5_reverse_chronological.csv', f'tests{path_slash}full_schafer5_reverse_chronological.csv')
+    os.replace('full_schafer5_reverse_chronological.md',  f'tests{path_slash}full_schafer5_reverse_chronological.md')
 
 
 def main():
