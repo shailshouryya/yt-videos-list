ab8bda848a998522b29e976ffab9432fbd81a887
Shail Shouryya <shailshouryya@gmail.com>
2020-06-15 21:29:40 -0700

    increases filepath route flexibility ('/' is Windows incompatible)

--- python3.6+/minifier.py
+++ python3.6+/minifier.py
@@ -23,13 +23,13 @@ def main():
             valid_files.append(filepath)
     for file in valid_files:
         with open(f'dev/{file}', 'r') as read_file, open(f'yt_videos_list{file}', 'w') as write_file:
-            if file.split('/')[-1] == '__init__.py':
+            if '__init__.py' in file:
                 write_file.write(read_file.read())
                 continue
             formatted = read_file.read()
             formatted = re.sub(r' +# .+', '', formatted)
             formatted = re.sub(r' +\n', '',  formatted)
-            if file.split('/')[-1] != 'notifications.py' and file.split('/')[-1] != 'write.py': formatted = re.sub(r'    ', ' ', formatted)
+            if 'notifications.py' not in file and 'write.py' not in file: formatted = re.sub(r'    ', ' ', formatted)
             formatted = re.sub(r'^\n', '',   formatted, flags=re.MULTILINE)
             write_file.write(formatted)
 
