b241f809c5f3a5a2573893620777819cc43550ad
Shail Shouryya <shailshouryya@gmail.com>
2020-06-22 23:33:29 -0700

    dynamically creates nested directories (❌ hard coding nested paths)

--- python3.6+/minifier.py
+++ python3.6+/minifier.py
@@ -15,16 +15,19 @@ def main():
     shutil.rmtree(f'./{target_directory}')
     time.sleep(2)
     os.mkdir(target_directory)
-    os.mkdir('yt_videos_list/download')
-    os.mkdir('yt_videos_list/file')
     shutil.move('temp.md', f'{readme}')
-    valid_files      = []
+    valid_files       = []
+    local_directories = set()
     for root, _, files in os.walk(os.path.abspath(f'./{source_directory}')):
         for file in files:
             filepath = os.path.join(root, file).split(source_directory)[1]
+            local_directory = root.split(source_directory)[1]
+            if local_directory: local_directories.add(local_directory) # tryuthy check skips adding an empty string '' to local_directories for files that are in root of source_directory
             if filepath.endswith('DS_Store'): continue
             if '__pycache__' in filepath:     continue
             valid_files.append(filepath)
+    for local_directory in local_directories:
+        os.makedirs(f'{target_directory}{local_directory}', exist_ok=True) # since sets are unordered, we might create a nested directory before the outer directory, so the exist_ok argument=True avoids this error -> FileExistsError: [Errno 17] File exists: '{local_directory}' (which is an outer directory for a nested directory that we made in the process of making a nested directory)
     for file in valid_files:
         with open(f'{source_directory}{slash}{file}', 'r') as read_file, open(f'{target_directory}{slash}{file}', 'w') as write_file:
             if '__init__.py' in file:
