07b207d654998fefc9c51c3a17a208ad98214041
Shail Shouryya <shailshouryya@gmail.com>
2020-06-24 00:29:03 -0700

    shortens argument name from "markdown" → "md" in interface
    - this change is simply a surface level change to make life easier for the end user
    - all under the hood references to "markdown" are still referred to as "markdown" for clarity in the codebase
      - since it's not as obvious what "md" might refer to as compared to "txt" & "csv"

--- python3.6+/dev/__init__.py
+++ python3.6+/dev/__init__.py
@@ -23,7 +23,7 @@ __source__               = 'https://github.com/Shail-Shouryya/yt_videos_list'
 
 
 class ListCreator:
-    def __init__(self, txt=True, csv=True, markdown=True, reverse_chronological=True, headless=False, scroll_pause_time=0.8, driver=None):
+    def __init__(self, txt=True, csv=True, md=True, reverse_chronological=True, headless=False, scroll_pause_time=0.8, driver=None):
         '''
         The ListCreator class creates a ListCreator instance with no required arguments.
         Example usage:
@@ -97,7 +97,7 @@ class ListCreator:
 
         self.txt                   = txt
         self.csv                   = csv
-        self.markdown              = markdown
+        self.markdown              = md
         self.reverse_chronological = reverse_chronological
         self.headless              = headless
         self.scroll_pause_time     = scroll_pause_time
