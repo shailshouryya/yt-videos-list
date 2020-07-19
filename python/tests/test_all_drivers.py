ba4fc93f9c513607df40598f2c8d41a2b61aaf03
Shail Shouryya <shailshouryya@gmail.com>
2020-07-19 03:17:26 -0700

    Order driver tests based on OS
    - trying to run msedgedriver on MacOS results in a sys.exit(), so the script will terminate as soon as it hits this test, so the
    msedgedriver should be the last test on a MacOS machine
    - trying to run safaridriver on Windows results in a sys.exit(), so the script will terminate as soon as it hits this test, so the
    safaridriver should be the last test on a Windows machine

--- python/tests/test_all_drivers.py
+++ python/tests/test_all_drivers.py
@@ -2,12 +2,17 @@ import test_cross_platform_drivers
 import test_safaridriver
 import test_msedgedriver
 
+from yt_videos_list.download.user_os_info import determine_user_os
 
 def main():
     test_cross_platform_drivers.main()
-    test_safaridriver.main()
-    test_msedgedriver.main()
-
+    user_os = determine_user_os()
+    if user_os == 'macos':
+        test_safaridriver.main()
+        test_msedgedriver.main()
+    elif user_os == 'windows':
+        test_msedgedriver.main()
+        test_safaridriver.main()
 
 if __name__ == '__main__':
     main()
