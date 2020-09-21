d906f90a6571eaf1695c5c601c2796b81a12969c
Shail Shouryya <shailshouryya@gmail.com>
2020-09-20 20:40:10 -0700

    Support operadriver v85 binaries

--- python/dev/notifications.py
+++ python/dev/notifications.py
@@ -91,6 +91,8 @@ class Common:
     @classmethod
     def format_operadriver_list(cls, operating_system):
         return [
+            cls.format_driver_information          (operating_system, 'v.85.0.4183.102', 'Stable 71', 'operadriver', 'Opera'),
+            cls.format_operadriver_download_command(operating_system, 'v.85.0.4183.102'),
             cls.format_driver_information          (operating_system, 'v.84.0.4147.89',  'Stable 70', 'operadriver', 'Opera'),
             cls.format_operadriver_download_command(operating_system, 'v.84.0.4147.89'),
             cls.format_driver_information          (operating_system, 'v.83.0.4103.97',  'Stable 69', 'operadriver', 'Opera'),
