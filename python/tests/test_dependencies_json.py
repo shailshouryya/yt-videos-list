8c73de6295edf461b64b594fef0f8a01470056bb
Shail Shouryya <shailshouryya@gmail.com>
2020-11-29 19:43:02 -0800

    Make PATH_SLASH a global variable

--- python/tests/test_dependencies_json.py
+++ python/tests/test_dependencies_json.py
@@ -5,6 +5,7 @@ os.system('pip install .')
 
 from yt_videos_list.notifications import Common
 from test_shared                  import determine_path_slash
+PATH_SLASH = determine_path_slash()
 
 
 def write_pseudo_json(drivers_dictionary):
@@ -21,8 +22,7 @@ def write_pseudo_json(drivers_dictionary):
         file.write('}')
 
 def format_pseudo_json():
-    path_slash = determine_path_slash()
-    with open('temp.json', 'r', encoding='utf-8') as ftemp, open(f'..{path_slash}docs{path_slash}dependencies_pseudo_json.txt', 'w', encoding='utf-8') as ffinal:
+    with open('temp.json', 'r', encoding='utf-8') as ftemp, open(f'..{PATH_SLASH}docs{PATH_SLASH}dependencies_pseudo_json.txt', 'w', encoding='utf-8') as ffinal:
         formatted = re.sub(',\n    ]', '\n    ]', ftemp.read())
         formatted = re.sub('],\n  },', ']\n  },', formatted)
         formatted = re.sub('},\n}',    '}\n}',    formatted)
@@ -33,11 +33,10 @@ def format_pseudo_json():
 def main():
     common_messages = Common()
     drivers_dictionary = common_messages.driver_downloads_for_os
-    path_slash = determine_path_slash()
 
     write_pseudo_json(drivers_dictionary)
     format_pseudo_json()
-    with open(f'..{path_slash}docs{path_slash}dependencies.json', 'w', encoding='utf-8') as ffinal:
+    with open(f'..{PATH_SLASH}docs{PATH_SLASH}dependencies.json', 'w', encoding='utf-8') as ffinal:
         json.dump(drivers_dictionary, ffinal, indent=4)
 
 
