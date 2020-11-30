6aa9c9439d392832016feb2342c164ba46ad9978
Shail Shouryya <shailshouryya@gmail.com>
2020-11-29 19:40:00 -0800

    Indicate function creates pseudo json

--- python/tests/test_dependencies_json.py
+++ python/tests/test_dependencies_json.py
@@ -7,7 +7,7 @@ from yt_videos_list.notifications import Common
 from test_shared                  import determine_path_slash
 
 
-def write_json(drivers_dictionary):
+def write_pseudo_json(drivers_dictionary):
     with open('temp.json', 'w', encoding='utf-8') as file:
         file.write('{\n')
         for driver in drivers_dictionary:
@@ -20,7 +20,7 @@ def write_json(drivers_dictionary):
             file.write('  },\n')
         file.write('}')
 
-def format_json():
+def format_pseudo_json():
     path_slash = determine_path_slash()
     with open('temp.json', 'r', encoding='utf-8') as ftemp, open(f'..{path_slash}docs{path_slash}dependencies_pseudo_json.txt', 'w', encoding='utf-8') as ffinal:
         formatted = re.sub(',\n    ]', '\n    ]', ftemp.read())
@@ -35,8 +35,8 @@ def main():
     drivers_dictionary = common_messages.driver_downloads_for_os
     path_slash = determine_path_slash()
 
-    write_json(drivers_dictionary)
-    format_json()
+    write_pseudo_json(drivers_dictionary)
+    format_pseudo_json()
     with open(f'..{path_slash}docs{path_slash}dependencies.json', 'w', encoding='utf-8') as ffinal:
         json.dump(drivers_dictionary, ffinal, indent=4)
 
