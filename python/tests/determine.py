87244a6463db2c0a18725e55165fcd8c7ff3d497
Shail Shouryya <shailshouryya@gmail.com>
2020-12-20 02:10:12 -0800

    Add docstrings for tests/determine.py

--- python/tests/determine.py
+++ python/tests/determine.py
@@ -4,16 +4,16 @@ import sys
 
 def determine_user_os():
     '''
-    This is the exact same function as the determine_user_os()
-    function in the download.user_os_info.py module. This
-    is included in a separate file under the tests/ directory
+    This is the exact same function as the `determine_user_os()`
+    function in the `download.user_os_info.py` module. This
+    is included in a separate file under the `tests/` directory
     to avoid the problem of importing the function from
-    yt_videos_list. Loading the function from the submodule
+    `yt_videos_list`. Loading the function from the submodule
     itself doesn't cause problems, but doing so prevents us
-    from reloading yt_videos_list into memory after performing
-    pip install .
+    from reloading `yt_videos_list` into memory after performing
+    `pip install .`
     or
-    pip3 install .
+    `pip3 install .` in the same python instance.
     '''
     user_os = platform.system().lower()
     if   user_os.startswith('darwin'):  return 'macos'
@@ -25,5 +25,12 @@ def determine_user_os():
 
 
 def determine_path_slash():
+    r'''
+    Returns the type of slash used in the file system of
+    the host machine. i.e.
+    Windows: '\\'
+    macOS:   '/'
+    Linux:   '/'
+    '''
     if determine_user_os() == 'windows': return '\\'
     else:                                return '/'
