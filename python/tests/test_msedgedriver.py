ff2564ef6cae480d6ca833ffb986a36e29427323
Shail Shouryya <shailshouryya@gmail.com>
2020-12-20 01:51:05 -0800

    Add docstrings for tests/test_msedgedriver.py

--- python/tests/test_msedgedriver.py
+++ python/tests/test_msedgedriver.py
@@ -1,3 +1,15 @@
+'''
+Test module for testing the selenium msedgedriver.
+The msedgedriver is currently only supported on
+Windows machines, so the `yt_videos_list` program
+should operate as expected on Windows machines,
+but exit with the following message on other
+systems:
+"ERROR! Selenium automation with msedgedriver
+(Microsoft Edge) is not yet supported on your
+platform. Please use a different browser!"
+'''
+
 if __name__ == '__main__':
     import os
     from determine import determine_user_os
@@ -15,6 +27,14 @@ from test_shared import run_tests_for
 
 
 def main():
+    '''
+    Tests the selenium msedgedriver on the host platform.
+    Program should operate as expected on Windows, but
+    terminate on other systems with the following:
+    "ERROR! Selenium automation with msedgedriver
+    (Microsoft Edge) is not yet supported on your
+    platform. Please use a different browser!"
+    '''
     browsers   = ['edge']
     run_tests_for(browsers)
 
