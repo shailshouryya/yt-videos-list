a2fbf768996851f44dc780f013509f465fe81838
Shail Shouryya <shailshouryya@gmail.com>
2020-12-19 22:08:29 -0800

    Add docstring for __init__ method

--- python/dev/__init__.py
+++ python/dev/__init__.py
@@ -138,6 +138,10 @@ class ListCreator:
       ===================================================
       '''
     def __init__(self, txt=True, csv=True, md=True, reverse_chronological=True, headless=False, scroll_pause_time=0.8, driver=None):
+        '''
+        Initialize an instance of ListCreator by setting values of the instance attributes as the provided arguments,
+        and setting any attributes not provided as the default value parameter value.
+        '''
         self.txt                   = txt
         self.csv                   = csv
         self.markdown              = md
