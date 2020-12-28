shailshouryya/yt-videos-list

commit 6fa0deb3d8b6c73612d79ea927f81f62e1141c29

Run "pip" on Windows and "pip3" on Unix

python/tests/test_cross_platform_drivers.py
@@ -1,9 +1,13 @@
 import os
-os.system('pip install .')
+
+from test_shared                          import create_test_cases, run_test_case, determine_path_slash
+formatted_pip = 'pip' if determine_path_slash() == '\\' else 'pip'
+os.system(f'{formatted_pip} install .')
+
 
 from yt_videos_list.download.windows_info import get_drive_letter
 from yt_videos_list.download.user_os_info import determine_user_os
-from test_shared                          import create_test_cases, run_test_case
+
 
 
 def remove_dependencies():

python/tests/test_dependencies_json.py
@@ -1,12 +1,14 @@
 import os
 import re
 import json
-os.system('pip install .')
+
+from test_shared                  import determine_path_slash
+PATH_SLASH    = determine_path_slash()
+formatted_pip = 'pip' if PATH_SLASH == '\\' else 'pip'
+os.system(f'{formatted_pip} install .')
+
 
 from yt_videos_list.notifications import Common
-from test_shared                  import determine_path_slash
-PATH_SLASH = determine_path_slash()
-
 
 def write_pseudo_json(drivers_dictionary):
     with open('temp.json', 'w', encoding='utf-8') as file:

commit 9550ca5b5c330238e94be8d4874fe0f4953731cb

Update local package without yt_videos_list submodule function
- addresses bug where version of yt_videos_list package loaded into memory was version right BEFORE running minifier.py
  - bug introduced in commit 6fa0deb3d8b6c73612d79ea927f81f62e1141c29
    - before, the test_cross_platform_drivers.py module ALWAYS ran `os.system(pip install .)` before ANY other imports, and this was sufficient to load the latest version of the package
    - relying on a module that DEPENDED on importing a function FROM yt_videos_list before trying to update yt_videos_list (in the same process) caused the problem
  - latest version of package wouldn't be loaded into memory because test_shared.py imported determine_user_os() FROM yt_videos_list.download.user_os_info
    - since test_shared.py had to load yt_videos_list to load a submodule from the package, the overall package couldn't be reloaded until restarting the python interpreter/process
- also greatly decreases the time it takes for minifier.py to run
  - likely because entire yt_videos_list package no longer needs to be loaded into memory just to run determine_path_slash()
  - decreases time minifier.py takes to run by 11x (ELEVEN TIMES!)
    - new import from test module            - average run time taken to run minifier 100 times (calculated from 10 loops of 100 iterations):  8.0744 seconds
    - old import from yt_videos-list package - average run time taken to run minifier 100 times (calculated from 10 loops of 100 iterations): 88.9669 seconds
      - 88.0744s / 88.9669s ≈ 11.018
      - tested on 2016 Macbook Pro using:
        - for i in {1..10}; do (time (for i in {1..100}; do python3 minifier.py; done)); done
    - see https://github.com/Shail-Shouryya/yt_videos_list/releases/tag/v0.5.0 for full details
- from the determine_user_os() docstring in the tests/determine module:

```
    This is the exact same function as the determine_user_os()
    function in the download.user_os_info.py module. This

python/dev/download/user_os_info.py
@@ -5,6 +5,8 @@ from ..notifications import Common
 
 
 def determine_user_os():
+    # make sure to update the determine_user_os() function
+    # in tests/deermine.py if anything is modified here!
     user_os = platform.system().lower()
     if   user_os.startswith('darwin'):  return 'macos'
     elif user_os.startswith('linux'):   return 'linux'

python/minifier.py
@@ -2,7 +2,7 @@ import os
 import re
 import shutil
 
-from tests.test_shared import determine_path_slash
+from tests.determine import determine_path_slash
 
 
 def clear_target_directory(target_directory):

python/tests/determine.py
@@ -1,0 +1,29 @@
+import platform
+import sys
+
+
+def determine_user_os():
+    '''
+    This is the exact same function as the determine_user_os()
+    function in the download.user_os_info.py module. This
+    is included in a separate file under the tests/ directory
+    to avoid the problem of importing the function from
+    yt_videos_list. Loading the function from the submodule
+    itself doesn't cause problems, but doing so prevents us
+    from reloading yt_videos_list into memory after performing
+    pip install .
+    or
+    pip3 install .
+    '''
+    user_os = platform.system().lower()
+    if   user_os.startswith('darwin'):  return 'macos'
+    elif user_os.startswith('linux'):   return 'linux'
+    elif user_os.startswith('windows'): return 'windows'
+    else:
+        print('Unsupported operating system, exiting tests...')
+        sys.exit()
+
+
+def determine_path_slash():
+    if determine_user_os() == 'windows': return '\\'
+    else:                                return '/'

python/tests/test_all_drivers.py
@@ -2,7 +2,7 @@ import test_cross_platform_drivers
 import test_safaridriver
 import test_msedgedriver
 
-from yt_videos_list.download.user_os_info import determine_user_os
+from determine import determine_user_os
 
 def main():
     test_cross_platform_drivers.main()

python/tests/test_cross_platform_drivers.py
@@ -1,8 +1,11 @@
 import os
 
-from test_shared                          import run_tests_for, determine_path_slash
-formatted_pip = 'pip' if determine_path_slash() == '\\' else 'pip'
-os.system(f'{formatted_pip} install .')
+from determine import determine_user_os
+USER_OS       = determine_user_os()
+FORMATTED_PIP = 'pip' if USER_OS == 'windows' else 'pip3'
+os.system(f'{FORMATTED_PIP} install .')
+
+from test_shared                          import run_tests_for
 
 
 from yt_videos_list.download.windows_info import get_drive_letter

python/tests/test_dependencies_json.py
@@ -2,10 +2,15 @@ import os
 import re
 import json
 
-from test_shared                  import determine_path_slash
-PATH_SLASH    = determine_path_slash()
-formatted_pip = 'pip' if PATH_SLASH == '\\' else 'pip'
-os.system(f'{formatted_pip} install .')
+from determine import determine_user_os
+USER_OS = determine_user_os()
+if USER_OS == 'windows':
+    FORMATTED_PIP = 'pip'
+    PATH_SLASH    = '\\'
+else:
+    FORMATTED_PIP = 'pip3'
+    PATH_SLASH    = '/'
+os.system(f'{FORMATTED_PIP} install .')
 
 
 from yt_videos_list.notifications import Common

python/tests/test_shared.py
@@ -5,13 +5,9 @@ import shutil
 import hashlib
 import threading
 
+from determine import determine_path_slash
+
 from yt_videos_list                       import ListCreator
-from yt_videos_list.download.user_os_info import determine_user_os
-
-
-def determine_path_slash():
-    if determine_user_os() == 'windows': return '\\'
-    else:                                return '/'
 
 
 def run_tests_for(browsers_list):

python/tests/update_reference_files.py
@@ -1,7 +1,8 @@
 import os
 
+from determine      import determine_path_slash
+
 from yt_videos_list import ListCreator
-from test_shared import determine_path_slash
 
 
 def update_schafer5_chronological():