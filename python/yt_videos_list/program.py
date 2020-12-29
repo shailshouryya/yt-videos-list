30e9701ed984122d61a28abc5b2598adcea60e93
Shail Shouryya <shailshouryya@gmail.com>
2020-12-28 21:01:31 -0800

    Make global varaibles local
    - leaving these variables as global variables won't cause any bugs since both of their values are a function, and the function is only called at run time as needed
      - however, these global variables are only used in one place, so setting them as global variables is useless
      - since global variables should only be used when the variables are used in multiple places with different scopes but need to have the same value

--- python/yt_videos_list/program.py
+++ python/yt_videos_list/program.py
@@ -3,9 +3,9 @@ import datetime
 from .     import file
 from .notifications import Common
 from .custom_logger import log
-COMMON_MESSAGE = Common()
-NOW   = datetime.datetime.now
 def determine_action(url, driver, scroll_pause_time, reverse_chronological, file_name, txt, csv, markdown, logging_output_location):
+ common_message = Common()
+ now   = datetime.datetime.now
  txt_exists = os.path.isfile(f'{file_name}.txt')
  csv_exists = os.path.isfile(f'{file_name}.csv')
  md_exists  = os.path.isfile(f'{file_name}.md')
@@ -15,14 +15,14 @@ def determine_action(url, driver, scroll_pause_time, reverse_chronological, file
  if txt_exists and csv_exists and md_exists: videos_list, txt_videos, csv_videos, md_videos = file.update_file.scroll_to_old_videos(url, driver, scroll_pause_time, file_name, logging_output_location)
  else:            videos_list         = file.create_file.scroll_to_bottom (url, driver, scroll_pause_time, logging_output_location)
  if len(videos_list) == 0:
-  log(COMMON_MESSAGE.no_videos_found, logging_output_location)
+  log(common_message.no_videos_found, logging_output_location)
   return
  if txt:
-  if txt_exists: file.update_file.write_to_txt(videos_list, file_name, reverse_chronological, logging_output_location, timestamp=NOW().isoformat().replace(':', '-').replace('.', '_'), stored_in_txt=txt_videos)
-  else:    file.create_file.write_to_txt(videos_list, file_name, reverse_chronological, logging_output_location, timestamp=NOW().isoformat().replace(':', '-').replace('.', '_'))
+  if txt_exists: file.update_file.write_to_txt(videos_list, file_name, reverse_chronological, logging_output_location, timestamp=now().isoformat().replace(':', '-').replace('.', '_'), stored_in_txt=txt_videos)
+  else:    file.create_file.write_to_txt(videos_list, file_name, reverse_chronological, logging_output_location, timestamp=now().isoformat().replace(':', '-').replace('.', '_'))
  if csv:
-  if csv_exists: file.update_file.write_to_csv(videos_list, file_name, reverse_chronological, logging_output_location, timestamp=NOW().isoformat().replace(':', '-').replace('.', '_'), stored_in_csv=csv_videos)
-  else:    file.create_file.write_to_csv(videos_list, file_name, reverse_chronological, logging_output_location, timestamp=NOW().isoformat().replace(':', '-').replace('.', '_'))
+  if csv_exists: file.update_file.write_to_csv(videos_list, file_name, reverse_chronological, logging_output_location, timestamp=now().isoformat().replace(':', '-').replace('.', '_'), stored_in_csv=csv_videos)
+  else:    file.create_file.write_to_csv(videos_list, file_name, reverse_chronological, logging_output_location, timestamp=now().isoformat().replace(':', '-').replace('.', '_'))
  if markdown:
-  if md_exists:  file.update_file.write_to_md (videos_list, file_name, reverse_chronological, logging_output_location, timestamp=NOW().isoformat().replace(':', '-').replace('.', '_'), stored_in_md=md_videos)
-  else:    file.create_file.write_to_md (videos_list, file_name, reverse_chronological, logging_output_location, timestamp=NOW().isoformat().replace(':', '-').replace('.', '_'))
+  if md_exists:  file.update_file.write_to_md (videos_list, file_name, reverse_chronological, logging_output_location, timestamp=now().isoformat().replace(':', '-').replace('.', '_'), stored_in_md=md_videos)
+  else:    file.create_file.write_to_md (videos_list, file_name, reverse_chronological, logging_output_location, timestamp=now().isoformat().replace(':', '-').replace('.', '_'))
