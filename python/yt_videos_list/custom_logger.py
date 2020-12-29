e7458d036eb5527afdf4eec50223c93ba7ada55a
Shail Shouryya <shailshouryya@gmail.com>
2020-12-28 21:08:35 -0800

    Make logging messages more visible

--- python/yt_videos_list/custom_logger.py
+++ python/yt_videos_list/custom_logger.py
@@ -5,5 +5,5 @@ def log(message, logging_output_location):
  thread_name = f'[{threading.current_thread().name}]'
  isoformat   = datetime.datetime.isoformat
  now   = datetime.datetime.now
- message  = f'{thread_name:>>14} {isoformat(now())}: {message}\n'
+ message  = f'===>{thread_name:>>14} {isoformat(now())}: {message}\n'
  logging_output_location.writelines(message)
