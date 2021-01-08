5424128fbe94fe463d03122da93eef19b264d87f
Shail Shouryya <shailshouryya@gmail.com>
2021-01-07 23:43:10 -0800

    Support operadriver v.87.0.4280.67 binaries

--- python/yt_videos_list/notifications.py
+++ python/yt_videos_list/notifications.py
@@ -77,6 +77,8 @@ class Common:
     @classmethod
     def format_operadriver_list(cls, operating_system):
         return [
+            cls.format_driver_information          (operating_system, 'v.87.0.4280.67',  'Stable 73', 'operadriver', 'Opera'),
+            cls.format_operadriver_download_command(operating_system, 'v.87.0.4280.67'),
             cls.format_driver_information          (operating_system, 'v.86.0.4240.80',  'Stable 72', 'operadriver', 'Opera'),
             cls.format_operadriver_download_command(operating_system, 'v.86.0.4240.80'),
             cls.format_driver_information          (operating_system, 'v.85.0.4183.102', 'Stable 71', 'operadriver', 'Opera'),
