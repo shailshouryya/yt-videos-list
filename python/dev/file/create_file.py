e020358cd73555ef9116ea8818663c6c9f112718
Shail Shouryya <shailshouryya@gmail.com>
2021-02-27 18:28:29 -0800

    Move all file creation logic → write.py

--- python/dev/file/create_file.py
+++ python/dev/file/create_file.py
@@ -1,11 +1,7 @@
-import functools
 import time
-import csv
 
-from .               import write
 from ..notifications import Common as common_message
-from ..custom_logger import log, log_extraction_information
-
+from ..custom_logger import log
 
 def scroll_to_bottom(url, driver, scroll_pause_time, logging_locations):
     start_time = time.perf_counter() # timer stops in save_elements_to_list() function
@@ -39,45 +35,3 @@ def save_elements_to_list(driver, start_time, scroll_pause_time, url, logging_lo
     total_time = end_time - start_time - scroll_pause_time # subtract scroll_pause_time to account for the extra waiting time to verify end of page
     log(f'It took {total_time} seconds to find all {len(elements)} videos from {url}\n', logging_locations)
     return elements
-
-
-def time_writer_function(writer_function):
-    @functools.wraps(writer_function)
-    def wrapper_timer(*args, **kwargs):
-        log_extraction_information(__name__, writer_function, args, kwargs)
-    return wrapper_timer
-
-
-@time_writer_function
-def write_to(file_type, list_of_videos, file_name, reverse_chronological, logging_locations, timestamp):
-    if file_type == 'csv': newline = ''
-    else:                  newline = None
-    csv_writer = None
-    with open(f'temp_{file_name}_{timestamp}.{file_type}', 'w', newline=newline, encoding='utf-8') as temp_file:
-        if file_type == 'csv':
-            fieldnames = ['Video Number', 'Video Title', 'Video URL', 'Watched?', 'Watch again later?', 'Notes']
-            csv_writer = csv.DictWriter(temp_file, fieldnames=fieldnames)
-            csv_writer.writeheader()
-        total_videos = txt_writer(file_type, temp_file, csv_writer, reverse_chronological, list_of_videos, logging_locations)
-    return file_name, total_videos, reverse_chronological, logging_locations
-
-
-def txt_writer(file_type, file, csv_writer, reverse_chronological, list_of_videos, logging_locations):
-    total_videos, total_writes, video_number, incrementer = prepare_output(list_of_videos, reverse_chronological)
-    for selenium_element in list_of_videos if reverse_chronological else list_of_videos[::-1]:
-        video_number, total_writes = write.entry(file_type, file, csv_writer, selenium_element, video_number, incrementer, total_writes)
-        if total_writes % 250 == 0:
-            log(f'{total_writes} videos written to {file.name}...', logging_locations)
-    return total_videos
-
-
-def prepare_output(list_of_videos, reverse_chronological):
-    total_videos = len(list_of_videos)
-    total_writes = 0
-    if reverse_chronological:
-        video_number = total_videos
-        incrementer  = -1
-    else:
-        video_number = 1
-        incrementer  = 1
-    return total_videos, total_writes, video_number, incrementer
--- python/dev/file/write.py
+++ python/dev/file/write.py
@@ -1,13 +1,61 @@
+import functools
+import csv
+
+from ..custom_logger import log, log_extraction_information
+
+
+def time_writer_function(writer_function):
+    @functools.wraps(writer_function)
+    def wrapper_timer(*args, **kwargs):
+        log_extraction_information(__name__, writer_function, args, kwargs)
+    return wrapper_timer
+
+
+@time_writer_function
+def create_file(file_type, list_of_videos, file_name, reverse_chronological, logging_locations, timestamp):
+    if file_type == 'csv': newline = ''
+    else:                  newline = None
+    csv_writer = None
+    with open(f'temp_{file_name}_{timestamp}.{file_type}', 'w', newline=newline, encoding='utf-8') as temp_file:
+        if file_type == 'csv':
+            fieldnames = ['Video Number', 'Video Title', 'Video URL', 'Watched?', 'Watch again later?', 'Notes']
+            csv_writer = csv.DictWriter(temp_file, fieldnames=fieldnames)
+            csv_writer.writeheader()
+        total_videos = create_writer(file_type, temp_file, csv_writer, reverse_chronological, list_of_videos, logging_locations)
+    return file_name, total_videos, reverse_chronological, logging_locations
+
+def create_writer(file_type, file, csv_writer, reverse_chronological, list_of_videos, logging_locations):
+    total_videos, total_writes, video_number, incrementer = prepare_output(list_of_videos, reverse_chronological)
+    for selenium_element in list_of_videos if reverse_chronological else list_of_videos[::-1]:
+        video_number, total_writes = entry(file_type, file, csv_writer, selenium_element, video_number, incrementer, total_writes)
+        if total_writes % 250 == 0:
+            log(f'{total_writes} videos written to {file.name}...', logging_locations)
+    return total_videos
+
+def prepare_output(list_of_videos, reverse_chronological):
+    total_videos = len(list_of_videos)
+    total_writes = 0
+    if reverse_chronological:
+        video_number = total_videos
+        incrementer  = -1
+    else:
+        video_number = 1
+        incrementer  = 1
+    return total_videos, total_writes, video_number, incrementer
+
+
 def update_status(video_number, total_writes, incrementer):
     video_number += incrementer
     total_writes += 1
     return video_number, total_writes
 
-def entry(file_type, file_object, csv_writer, selenium_element, video_number, incrementer, total_writes):
-    if file_type == 'csv': return csv_entry(csv_writer, selenium_element, video_number, incrementer, total_writes)
-    else:                  return txt_entry(file_object, selenium_element, video_number, incrementer, total_writes, file_type)
 
-def txt_entry(file, selenium_element, video_number, incrementer, total_writes, file_type):
+
+def entry(file_type, file_object, csv_writer, selenium_element, video_number, incrementer, total_writes):
+    if file_type == 'csv': return write_csv (csv_writer,  selenium_element, video_number, incrementer, total_writes)
+    else:                  return write_text(file_object, selenium_element, video_number, incrementer, total_writes, file_type)
+
+def write_text(file, selenium_element, video_number, incrementer, total_writes, file_type):
     newline = '\n'
     md      = file_type == 'md'
     if md: spacing = f'{newline}' + '- ' + f'{newline}'
@@ -25,7 +73,7 @@ def txt_entry(file, selenium_element, video_number, incrementer, total_writes, f
     file.write('*'*75 + newline)
     return update_status(video_number, total_writes, incrementer)
 
-def csv_entry(writer, selenium_element, video_number, incrementer, total_writes):
+def write_csv(writer, selenium_element, video_number, incrementer, total_writes):
     writer.writerow(
         {
             'Video Number':      f'{video_number}',
