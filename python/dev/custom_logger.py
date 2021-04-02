9e980d6b255ef811ec25ddd331eb764958efd13d
Shail Shouryya <shailshouryya@gmail.com>
2021-04-01 19:50:53 -0700

    Remove unnecessary f-string (linter error)
    - addresses the following pylint error:
      - W1309: Using an f-string that does not have any interpolated variables (f-string-without-interpolation)

--- python/dev/custom_logger.py
+++ python/dev/custom_logger.py
@@ -27,11 +27,11 @@ def log_extraction_information(function, writer_function, args, kwargs):
     total_time = end_time - start_time
     temp_file  = f'temp_{file_name}_{timestamp}.{extension}'    # determine temp_{file_name} for wrapper_timer() scope (writer_function defines it in its own scope already)
     final_file = f'{file_name}.{extension}'
-    log(f'Finished writing to'.ljust(39) + f'{temp_file}',                                                          logging_locations)
+    log('Finished writing to'.ljust(39) + f'{temp_file}',                                                           logging_locations)
     if function == 'create_file': log(f'{videos_written} {videos} written to'.ljust(39) + f'{temp_file}',           logging_locations)
     if function == 'update_file': log(f'{videos_written} ***NEW*** {videos} written to'.ljust(39) + f'{temp_file}', logging_locations)
-    log(f'Closing'.ljust(39) + f'{temp_file}',                                                                      logging_locations)
-    log(f'Successfully completed write, renaming {temp_file} to {final_file}',                                      logging_locations)
+    log('Closing'.ljust(39) + f'{temp_file}',                                                                       logging_locations)
+    log('Successfully completed write, renaming {temp_file} to {final_file}',                                       logging_locations)
     if function == 'update_file' and not reverse_chronological: # ChannelName_chronological.ext files
         # remove temp_{file_name} since all new information from the temp file was appended to the end of the original file
         os.remove(temp_file)
@@ -39,6 +39,6 @@ def log_extraction_information(function, writer_function, args, kwargs):
         # for the create_file function, rename temp_{file_name} to {file_name}.{extension} here AFTER everything else finishes to ensure atomicity (since final file could be written to directly, there's no need for a temp file as in update_file.py)
         # for the update_file function, rename temp_{file_name} to {file_name}.{extension} since the info from the original file was appended to the end of the temp file
         os.replace(temp_file, final_file)
-    log(f'Successfully renamed'.ljust(39) + f'{temp_file} to {final_file}',                                                                                        logging_locations)
+    log('Successfully renamed'.ljust(39) + f'{temp_file} to {final_file}',                                                                                         logging_locations)
     if function == 'create_file': log(f'It took {total_time} seconds to write all {videos_written} {videos} to {final_file}{NEWLINE}',                             logging_locations)
     if function == 'update_file': log(f'It took {total_time} seconds to write the {videos_written} ***NEW*** {videos} to the pre-existing {final_file} {NEWLINE}', logging_locations)
