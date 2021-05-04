98cc7783b717a2626e953d3a7d79238c7f0b091a
Shail Shouryya <shailshouryya@gmail.com>
2021-05-03 22:18:13 -0700

    Remove extra newline in main() (minifier.py)

--- python/minifier.py
+++ python/minifier.py
@@ -15,7 +15,6 @@ def main():
     slash            = determine_path_slash()
     source_directory = 'dev'
     target_directory = 'yt_videos_list'
-
     clear_target_directory(target_directory)
     minify_source_directory_into_target_directory(slash, source_directory, target_directory)
 
