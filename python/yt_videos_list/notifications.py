e446cada8bdecb0973018398f0d067d003c34471
Shail Shouryya <shailshouryya@gmail.com>
2021-05-05 21:11:20 -0700

    Update msedgedriver v90 binaries (90.0.803.0 → 90.0.818.41)

--- python/yt_videos_list/notifications.py
+++ python/yt_videos_list/notifications.py
@@ -217,8 +217,8 @@ class Common:
  @classmethod
  def format_msedgedriver_list(cls, operating_system):
   return [
-   cls.format_driver_information (operating_system, '90.0.803.0', '90', 'msedgedriver', 'Microsoft Edge'),
-   cls.format_msedgedriver_download_command(operating_system, '90.0.803.0'),
+   cls.format_driver_information (operating_system, '90.0.818.41', '90', 'msedgedriver', 'Microsoft Edge'),
+   cls.format_msedgedriver_download_command(operating_system, '90.0.818.41'),
    cls.format_driver_information (operating_system, '89.0.774.77', '89', 'msedgedriver', 'Microsoft Edge'),
    cls.format_msedgedriver_download_command(operating_system, '89.0.774.77'),
    cls.format_driver_information (operating_system, '88.0.705.74', '88', 'msedgedriver', 'Microsoft Edge'),
