ada15bcdf9dd9a28f7147b338095ca47b7dfdc8d
Shail Shouryya <shailshouryya@gmail.com>
2021-05-09 23:17:00 -0700

    0.5.6: Support newer (AND much older) driver binaries
    - Mozilla Firefox
      - geckodriver v0.29.1 (Firefox versions ≥ 87)
    - Opera Stable 75, 76 & older binaries
      - operadriver v.90.0.4430.85 (Opera Stable 76)
      - operadriver v.89.0.4389.82 (Opera Stable 75)
      - operadriver v.2.36 (Opera 53)
      - operadriver v.2.35 (Opera 52)
      - operadriver v.2.33 (Opera 50)
      - operadriver v.2.32 (Opera 49)
      - operadriver v.2.30 (Opera 48)
      - operadriver v.2.29 (Opera 46)
    - Google Chrome version 89, 90, 91 & older binaries
      - chromedriver 91.0.4472.19
      - chromedriver 90.0.4430.24
      - chromedriver 89.0.4389.23
      - chromedriver 2.45 (v70-72)
      - chromedriver 2.44 (v69-71)
      - chromedriver 2.43 (v69-71) - use chromedriver 2.44
      - chromedriver 2.42 (v68-70)
      - chromedriver 2.41 (v67-69)
    - Brave Browser version 89, 90
      - bravedriver v.90.0.4430.85 (uses operadriver binaries)
      - bravedriver v.89.0.4389.82 (uses operadriver binaries)
    - Microsoft Edge version 91, 92 (updated version 89 & 90 binaries)
      - msedgedriver 92.0.881.0
      - msedgedriver 91.0.864.19
      - msedgedriver 90.0.818.56 (previously 90.0.803.0)
      - msedgedriver 89.0.774.77 (previously 89.0.774.27)

--- python/dev/__init__.py
+++ python/dev/__init__.py
@@ -9,7 +9,7 @@ from .              import logic
 from .notifications import Common, ModuleMessage
 
 
-__version__              = '0.5.5'
+__version__              = '0.5.6'
 __author__               = 'Shail-Shouryya'
 __email__                = 'yt.videos.list@gmail.com'
 __development_status__   = '4 - Beta'
