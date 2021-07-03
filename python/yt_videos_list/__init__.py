916f0502420f9f9ccc64b370cf6f531cad6f24c7
Shail Shouryya <shailshouryya@gmail.com>
2021-07-02 22:54:21 -0700

    Change default `verify_page_bottom_n_times` value to `3`

--- python/yt_videos_list/__init__.py
+++ python/yt_videos_list/__init__.py
@@ -137,7 +137,7 @@ class ListCreator:
     | Thank you!!                                       |
     =====================================================
     '''
-    def __init__(self, txt=True, csv=True, md=True, reverse_chronological=True, headless=False, scroll_pause_time=0.8, driver=None, cookie_consent=False, verify_page_bottom_n_times=1, file_buffering=-1):
+    def __init__(self, txt=True, csv=True, md=True, reverse_chronological=True, headless=False, scroll_pause_time=0.8, driver=None, cookie_consent=False, verify_page_bottom_n_times=3, file_buffering=-1):
         '''
         Initializes an instance of ListCreator by setting the attributes of the instance to the provided arguments,
         and setting any attributes not provided as the default parameter value.
