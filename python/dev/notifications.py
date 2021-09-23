e1e48c0b5e230ad0d3b1dff86b985ce7c15bfb61
slow-but-steady <slowbutsteady1234@gmail.com>
2021-09-22 22:35:32 -0700

    Support operadriver v.92.0.4515.107 (Opera Stable 78) binaries

--- python/dev/notifications.py
+++ python/dev/notifications.py
@@ -95,6 +95,8 @@ class Common:
     @classmethod
     def format_operadriver_list(cls, operating_system):
         return [
+            cls.format_driver_information          (operating_system, 'v.92.0.4515.107', 'Stable 78', 'operadriver', 'Opera'),
+            cls.format_operadriver_download_command(operating_system, 'v.92.0.4515.107'),
             cls.format_driver_information          (operating_system, 'v.91.0.4472.77',  'Stable 77', 'operadriver', 'Opera'),
             cls.format_operadriver_download_command(operating_system, 'v.91.0.4472.77'),
             cls.format_driver_information          (operating_system, 'v.90.0.4430.85',  'Stable 76', 'operadriver', 'Opera'),
