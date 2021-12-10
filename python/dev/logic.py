d6c6e1a5c17b59f782eece2190c0a5e072001d51
slow-but-steady <slowbutsteady1234@gmail.com>
2021-12-09 23:31:30 -0800

    Left pad thread name with 12 spaces (logic.py)
    - aligns text displayed after thread name
      - previously, text alignment became a problem if not all threads had the same number of digits
        - Thread-10 <-- has 2 digits
        - Thread-11 <-- has 2 digits
        - Thread-9  <-- has 1 digit
          - due to the mismatch in number of digits, the text that followed was no longer aligned

--- python/dev/logic.py
+++ python/dev/logic.py
@@ -315,9 +315,9 @@ def execute(urls, file_name, log_silently, txt, csv, markdown, file_suffix, all_
             program_start = time.perf_counter()
             if urls: url = urls.popleft()
             else:    continue
-            if aggregate_logging_locations: log(f'{thread_name} {" "*8} scraping {count:>7}: {url}', aggregate_logging_locations)
+            if aggregate_logging_locations: log(f'{thread_name:<12} {" "*8} scraping {count:>7}: {url}', aggregate_logging_locations)
             url = process_url()
             video_data, write_information, thread_running_time = run_scraper()
             channel_name, output_file_name                     = write_information
-            if aggregate_logging_locations: log(f'{thread_name} finished scraping {count:>7}: "{channel_name}" and wrote to the {output_file_name} file in {thread_running_time} seconds', aggregate_logging_locations)
+            if aggregate_logging_locations: log(f'{thread_name:<12} finished scraping {count:>7}: "{channel_name}" and wrote to the {output_file_name} file in {thread_running_time} seconds', aggregate_logging_locations)
         return (video_data, (channel_name, output_file_name))
