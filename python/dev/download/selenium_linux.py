36d6b5b32417bb4b05722a4ba37bf2b14eca87e4
slow-but-steady <slowbutsteady1234@gmail.com>
2022-02-03 20:54:10 -0800

    Clarify selenium_linux submodule `get_browser_version()` `RuntimeError` message

--- python/dev/download/selenium_linux.py
+++ python/dev/download/selenium_linux.py
@@ -6,4 +6,4 @@ def get_browser_version(browser):
     # with open(f'/Applications/{browser}.app/Contents/Info.plist', mode='r', encoding='utf-8') as f:
     #     info_plist = f.read()
     # return re.search('<key>CFBundleShortVersionString</key>\s*<string>([0-9\.]+)', info_plist)[1]
-    raise RuntimeError('Automatic selenium updates are not yet available for Linux distributions!\nPlease update your selenium driver manually.')
+    raise RuntimeError('Browser version detection and automatic selenium updates are not yet available for Linux distributions!\nPlease update your selenium driver manually.')
--- python/yt_videos_list/download/selenium_linux.py
+++ python/yt_videos_list/download/selenium_linux.py
@@ -1,4 +1,4 @@
 def browser_exists(browser):
  raise RuntimeError('Browser detection and automatic selenium updates are not yet available for Linux distributions!\nPlease update your selenium driver manually.')
 def get_browser_version(browser):
- raise RuntimeError('Automatic selenium updates are not yet available for Linux distributions!\nPlease update your selenium driver manually.')
+ raise RuntimeError('Browser version detection and automatic selenium updates are not yet available for Linux distributions!\nPlease update your selenium driver manually.')
