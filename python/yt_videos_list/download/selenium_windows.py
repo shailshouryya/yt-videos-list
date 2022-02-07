7939b80dd28c2875a3ba93c4cc4cd03618086e27
slow-but-steady <slowbutsteady1234@gmail.com>
2022-02-06 20:53:55 -0800

    Rename helper functions by prepending `verify_` to the function name
    - similar to the previous commit:
      - this change makes the function naming consistent with the convention of using a verb phrase for a function name

--- python/yt_videos_list/download/selenium_windows.py
+++ python/yt_videos_list/download/selenium_windows.py
@@ -6,23 +6,23 @@ from .user_os_info import determine_user_os
 if determine_user_os() == 'windows':
  DRIVE = get_drive_letter()
  USER = get_user_name()
-def firefox_exists(browser):
+def verify_firefox_exists(browser):
  return browser in subprocess.getoutput(rf'dir "{DRIVE}:\Program Files"')
-def opera_exists(browser):
+def verify_opera_exists(browser):
  return browser in subprocess.getoutput(rf'dir {DRIVE}:\Users\{USER}\AppData\Local\Programs')
-def chrome_exists(browser):
+def verify_chrome_exists(browser):
  return browser in subprocess.getoutput(rf'dir "{DRIVE}:\Program Files (x86)\Google"')
-def brave_exists(browser):
+def verify_brave_exists(browser):
  return browser in subprocess.getoutput(rf'dir "{DRIVE}:\Program Files (x86)/BraveSoftware"') or browser in subprocess.getoutput(rf'dir "{DRIVE}:\Program Files/BraveSoftware"')
-def edge_exists(browser):
+def verify_edge_exists(browser):
  return browser in subprocess.getoutput(rf'dir "{DRIVE}:\Program Files (x86)/Microsoft"')
 def verify_browser_exists(browser):
  return {
-  'Mozilla Firefox': firefox_exists(browser),
-  'Opera': opera_exists(browser),
-  'Chrome': chrome_exists(browser),
-  'Brave-Browser': brave_exists(browser),
-  'Edge': edge_exists(browser)
+  'Mozilla Firefox': verify_firefox_exists(browser),
+  'Opera': verify_opera_exists(browser),
+  'Chrome': verify_chrome_exists(browser),
+  'Brave-Browser': verify_brave_exists(browser),
+  'Edge': verify_edge_exists(browser)
  }[browser]
 def get_firefox_version():
  firefox = subprocess.getoutput(rf'more "{DRIVE}:\Program Files\Mozilla Firefox\application.ini"')
