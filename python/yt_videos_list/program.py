560b09c5e53eb4ee2404cb5e6b34ae0917887ea3
slow-but-steady <slowbutsteady1234@gmail.com>
2022-03-18 20:50:40 -0700

    Rename variable `total_cpu_time` → `video_loading_cpu_time` for clarity

--- python/yt_videos_list/program.py
+++ python/yt_videos_list/program.py
@@ -104,9 +104,9 @@ def load_video_data(videos_list, common_visited_videos, video_id_only, reverse_c
   video_data.reverse()
  video_loading_cpu_end_time = time.perf_counter()
  end_real_time = time.time()
- total_cpu_time = video_loading_cpu_end_time - video_loading_cpu_start_time
+ video_loading_cpu_time = video_loading_cpu_end_time - video_loading_cpu_start_time
  total_real_time = end_real_time - start_real_time
- log(f'It took {total_cpu_time} seconds ({total_real_time} seconds real time) to load information for {videos_to_load} videos into memory\n', logging_locations)
+ log(f'It took {video_loading_cpu_time} seconds ({total_real_time} seconds real time) to load information for {videos_to_load} videos into memory\n', logging_locations)
  if video_id_only is True:
   log('Keeping only the video ID from the full video URL...', logging_locations)
   for video_datum in video_data:
