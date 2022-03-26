4a2b2eb08765e4a428d86354ac318a19aebb874d
slow-but-steady <slowbutsteady1234@gmail.com>
2022-03-25 20:50:05 -0700

    Rename variable `start_real_time` → `scrolling_real_start_time` for clarity

--- python/dev/scroller.py
+++ python/dev/scroller.py
@@ -9,7 +9,7 @@ def scroll_until_break(url, driver, scroll_pause_time, logging_locations, verify
     if force_to_page_bottom: visited_videos.clear()                                  # clear any pre-existing video information if there are pre-existing files (will already be empty if there are no pre-existing files)
     else:                    verify_page_bottom_n_times       *= 3                   # it is VERY unlikely that a pre-existing file exists and the program reaches the end of the page before finding ANY pre-existing vides, so increase value for break condition by 3 to make sure this is actually the case and not a false positive
     scrolling_cpu_start_time                                   = time.perf_counter() # timer stops in save_elements_to_list() function
-    start_real_time                                            = time.time()         # this timer also stops in save_elements_to_list() function
+    scrolling_real_start_time                                  = time.time()         # this timer also stops in save_elements_to_list() function
     current_elements_count                                     = None
     new_elements_count                                         = count_videos_on_page(driver)
     num_times_elements_count_same                              = -1
@@ -25,7 +25,7 @@ def scroll_until_break(url, driver, scroll_pause_time, logging_locations, verify
             if url_of_last_loaded_video_on_page() in visited_videos:
                 # if force_to_page_bottom is True, visited_videos will be an empty set and this conditional will never execute
                 found_old_videos = True
-    return save_elements_to_list(driver, scrolling_cpu_start_time, start_real_time, url, logging_locations), stored_in_txt, stored_in_csv, stored_in_md, visited_videos
+    return save_elements_to_list(driver, scrolling_cpu_start_time, scrolling_real_start_time, url, logging_locations), stored_in_txt, stored_in_csv, stored_in_md, visited_videos
 
 
 
@@ -100,11 +100,11 @@ def verify_reached_page_bottom(new_elements_count, current_elements_count, num_t
         num_times_elements_count_same = -1
     return num_times_elements_count_same
 
-def save_elements_to_list(driver, scrolling_cpu_start_time, start_real_time, url, logging_locations):
+def save_elements_to_list(driver, scrolling_cpu_start_time, scrolling_real_start_time, url, logging_locations):
     elements   = driver.find_elements_by_xpath('//*[@id="video-title"]')
     scrolling_cpu_end_time   = time.perf_counter()
     end_real_time = time.time()
     scrolling_cpu_time = scrolling_cpu_end_time - scrolling_cpu_start_time
-    total_real_time = end_real_time - start_real_time
+    total_real_time = end_real_time - scrolling_real_start_time
     log(f'It took {scrolling_cpu_time} seconds ({total_real_time} seconds real time)) to find {len(elements)} videos from {url}\n', logging_locations)
     return elements
