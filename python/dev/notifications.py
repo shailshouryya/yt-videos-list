713d45c405ffb435bae891e4c2e58b5a5a2fc34f
slow-but-steady <slowbutsteady1234@gmail.com>
2022-08-21 19:45:30 -0700

    Support operadriver v.97.0.4692.71 (Opera Stable 83) binaries

--- python/dev/notifications.py
+++ python/dev/notifications.py
@@ -104,6 +104,8 @@ class Common:
         driver_name  = 'operadriver'
         browser_name = 'Opera Browser'
         return [
+            cls.format_driver_information           (operating_system, 'v.97.0.4692.71',  'Stable 83', driver_name, browser_name),
+            cls.format_download_command(driver_name, operating_system, 'v.97.0.4692.71'),
             cls.format_driver_information           (operating_system, 'v.96.0.4664.45',  'Stable 82', driver_name, browser_name),
             cls.format_download_command(driver_name, operating_system, 'v.96.0.4664.45'),
             cls.format_driver_information           (operating_system, 'v.95.0.4638.54',  'Stable 81', driver_name, browser_name),
