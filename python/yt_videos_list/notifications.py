d259418564689150fdb831712929666d87bcfbfa
slow-but-steady <slowbutsteady1234@gmail.com>
2022-08-23 19:45:20 -0700

    Support operadriver v.98.0.4758.82 (Opera Stable 84) binaries

--- python/yt_videos_list/notifications.py
+++ python/yt_videos_list/notifications.py
@@ -95,6 +95,8 @@ class Common:
   driver_name = 'operadriver'
   browser_name = 'Opera Browser'
   return [
+   cls.format_driver_information (operating_system, 'v.98.0.4758.82', 'Stable 84', driver_name, browser_name),
+   cls.format_download_command(driver_name, operating_system, 'v.98.0.4758.82'),
    cls.format_driver_information (operating_system, 'v.97.0.4692.71', 'Stable 83', driver_name, browser_name),
    cls.format_download_command(driver_name, operating_system, 'v.97.0.4692.71'),
    cls.format_driver_information (operating_system, 'v.96.0.4664.45', 'Stable 82', driver_name, browser_name),
