b63a174cf24367aa81c77f382369a318b9b37d2e
slow-but-steady <slowbutsteady1234@gmail.com>
2022-08-26 19:45:05 -0700

    Support bravedriver v.99.0.4844.51 (operadriver based) binaries

--- python/dev/notifications.py
+++ python/dev/notifications.py
@@ -257,6 +257,8 @@ class Common:
         driver_name  = 'bravedriver (operadriver based)'
         browser_name = 'Brave Browser'
         return [
+            cls.format_driver_information           (operating_system, 'v.99.0.4844.51',  '99', driver_name, browser_name),
+            cls.format_download_command(driver_name, operating_system, 'v.99.0.4844.51'),
             cls.format_driver_information           (operating_system, 'v.98.0.4758.82',  '98', driver_name, browser_name),
             cls.format_download_command(driver_name, operating_system, 'v.98.0.4758.82'),
             cls.format_driver_information           (operating_system, 'v.97.0.4692.71',  '97', driver_name, browser_name),
