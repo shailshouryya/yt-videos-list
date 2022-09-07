22530d2dc24616ea8812ac6a2c72ce3a1d63cc2a
slow-but-steady <slowbutsteady1234@gmail.com>
2022-09-06 19:44:10 -0700

    Support chromedriver 102.0.5005.61 binaries

--- python/dev/notifications.py
+++ python/dev/notifications.py
@@ -194,6 +194,8 @@ class Common:
         driver_name  = 'chromedriver'
         browser_name = 'Google Chrome'
         return [
+            cls.format_driver_information           (operating_system, '102.0.5005.61',   '102', driver_name, browser_name),
+            cls.format_download_command(driver_name, operating_system, '102.0.5005.61'),
             cls.format_driver_information           (operating_system, '101.0.4951.41',   '101', driver_name, browser_name),
             cls.format_download_command(driver_name, operating_system, '101.0.4951.41'),
             cls.format_driver_information           (operating_system, '100.0.4896.60',   '100', driver_name, browser_name),
