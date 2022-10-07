f8ccec53d4e5f15ea79a1be3435ba1663acdeed4
slow-but-steady <slowbutsteady1234@gmail.com>
2022-10-06 21:12:40 -0700

    Support msedgedriver 103.0.1264.77 binaries

--- python/dev/notifications.py
+++ python/dev/notifications.py
@@ -354,6 +354,8 @@ class Common:
         driver_name  = 'msedgedriver'
         browser_name = 'Microsoft Edge'
         return [
+            cls.format_driver_information           (operating_system, '103.0.1264.77',    '103', driver_name, browser_name),
+            cls.format_download_command(driver_name, operating_system, '103.0.1264.77'),
             cls.format_driver_information           (operating_system, '102.0.1245.62',    '102', driver_name, browser_name),
             cls.format_download_command(driver_name, operating_system, '102.0.1245.62'),
             cls.format_driver_information           (operating_system, '101.0.1210.53',    '101', driver_name, browser_name),
