a59a7ad4740aba2514b43b8f070880ea3fb120a1
slow-but-steady <slowbutsteady1234@gmail.com>
2022-11-10 20:47:00 -0800

    Support msedgedriver 107.0.1418.42 binaries

--- python/yt_videos_list/notifications.py
+++ python/yt_videos_list/notifications.py
@@ -339,6 +339,8 @@ class Common:
   driver_name = 'msedgedriver'
   browser_name = 'Microsoft Edge'
   return [
+   cls.format_driver_information (operating_system, '107.0.1418.42', '107', driver_name, browser_name),
+   cls.format_download_command(driver_name, operating_system, '107.0.1418.42'),
    cls.format_driver_information (operating_system, '106.0.1370.52', '106', driver_name, browser_name),
    cls.format_download_command(driver_name, operating_system, '106.0.1370.52'),
    cls.format_driver_information (operating_system, '105.0.1343.53', '105', driver_name, browser_name),
