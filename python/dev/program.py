ca31f62488b458d836a7349163f36e1fb778c8f9
slow-but-steady <slowbutsteady1234@gmail.com>
2022-12-21 23:20:07 -0800

    Move video title formatting into `normalize_whitespace` function
    - moving this code into a separate function enables easier
      functionality testing

--- python/dev/program.py
+++ python/dev/program.py
@@ -107,8 +107,7 @@ def load_video_data(videos_list, common_visited_videos, video_id_only, reverse_c
     videos_to_load = video_number
     for videos_loaded, selenium_element in enumerate(videos_list, start=1):
         video_title    = selenium_element.get_attribute('title')
-        video_title    = re.sub('[\n\r]+', ' ', video_title)
-        video_title    = re.sub('\s{2,}', ' ', video_title).strip()
+        video_title    = normalize_whitespace(video_title)
         video_url      = selenium_element.get_attribute('href').replace('shorts/', 'watch?v=').split('&pp')[0]
         try:
             video_duration = selenium_element.find_element_by_xpath('./../../../../ytd-thumbnail/a[@id="thumbnail"]/div[@id="overlays"]/ytd-thumbnail-overlay-time-status-renderer/span').get_attribute('innerHTML').split()[0]
@@ -136,3 +135,9 @@ def load_video_data(videos_list, common_visited_videos, video_id_only, reverse_c
             video_datum[3] = video_id
         log('Finished formatting the video IDs...\n', logging_locations)
     return video_data
+
+
+def normalize_whitespace(raw_text):
+    text_with_newline_and_cr_removed = re.sub('[\n\r]+', ' ', raw_text)
+    normalized_text                  = re.sub('\s{2,}', ' ', text_with_newline_and_cr_removed).strip()
+    return normalized_text
---
+++ python/tests/test_units.py
@@ -0,0 +1,39 @@
+from yt_videos_list.program import normalize_whitespace
+
+
+def main():
+    test_normalize_whitespace()
+
+def test_normalize_whitespace():
+    test_cases =     test_cases = (
+        ('This is a title with no newlines', 'This is a title with no newlines'),
+        ('This is a title with one newline \n', 'This is a title with one newline'),
+        ('This is a title with two newlines \n\n', 'This is a title with two newlines'),
+        ('This is a title with two\n\nnewlines', 'This is a title with two newlines'),
+        ('This is a title with\n\ntwo newlines', 'This is a title with two newlines'),
+        ('This is a title with one newline \n and one carriage return\r', 'This is a title with one newline and one carriage return'),
+        ('This is a title with one newline \n and \rone carriage return', 'This is a title with one newline and one carriage return'),
+        ('This is a title with one newline \n\r and one carriage return', 'This is a title with one newline and one carriage return'),
+        ('This is a title with one newline \r\n and one carriage return', 'This is a title with one newline and one carriage return'),
+        ('This is a title with one newline \r \n and one carriage return', 'This is a title with one newline and one carriage return'),
+        ('This is a title with one newline \r \n and one carriage return', 'This is a title with one newline and one carriage return'),
+        ('This is a title with multiple newlines \r \n and multiple carriage returns \r\r', 'This is a title with multiple newlines and multiple carriage returns'),
+        ('This is a title with multiple newlines \r \n and multiple carriage returns \n\n', 'This is a title with multiple newlines and multiple carriage returns'),
+        ('This is a title with multiple newlines \r \n and multiple carriage returns \r\n', 'This is a title with multiple newlines and multiple carriage returns'),
+        ('This is a title with multiple newlines \r\n and multiple carriage returns \r\n', 'This is a title with multiple newlines and multiple carriage returns'),
+        ('This is a title with multiple newlines \n\r and multiple carriage returns \r\n', 'This is a title with multiple newlines and multiple carriage returns'),
+    )
+    error_message = f'''
+    Test case {index} is not properly formatted!
+    The raw_text provided was: {raw_text}
+    The expected normalized_text is: {normalized_text}
+    The actual normalized_text output is: {actual_output_text}
+    '''.format(index, raw_text, normalized_text, actual_output_text)
+    for index, raw_text, normalized_text in enumerate(test_cases):
+        actual_output_text = normalize_whitespace(raw_text)
+        if actual_output_text != normalized_text:
+            raise ValueError(error_message)
+
+
+if __name__ == '__main__':
+    main()
